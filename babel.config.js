b64f719fb06b6b0453d95737d9577e3d129fc861
xilanhuadechuntian <Lensgcx@163.com>
2019-08-01 09:58:24 +0800

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,37 @@
+const useESModules = process.env.BABEL_MODULE !== 'commonjs';
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: useESModules ? false : 'commonjs',
+        targets: {
+          browsers: [
+            '> 1%',
+            'last 2 versions',
+            'not ie <= 8',
+          ],
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-transform-modules-commonjs',
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    ['@babel/plugin-proposal-class-properties', {loose: true}],
+    '@babel/plugin-syntax-jsx',
+    '@vue/babel-plugin-transform-vue-jsx',
+    'jsx-v-model',
+  ],
+  env: {
+    development: {
+      plugins: [
+        'dynamic-import-node',
+      ],
+    },
+  },
+};
