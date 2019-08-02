c6971ea8e596c282a74087c9d3c4c5056eeab95c
xilanhuadechuntian <Lensgcx@163.com>
2019-08-02 09:02:55 +0800

    Update table-fetch.js

--- main/mainstay/service/System/TableFetch/table-fetch.js
+++ main/mainstay/service/System/TableFetch/table-fetch.js
@@ -1,15 +1,4 @@
 import fetch from '../../../utils/fetch/fetch';
-import { dataFilter } from '../../../utils/fetch/fetchDataType';
-
-export function listColumnService(head) {
-  dataFilter(head);
-  return fetch({
-    url: '/system/column/list-column',
-    method: 'get',
-    headers: head,
-  });
-}
-
 
 /**
  * table data fetch
@@ -24,7 +13,7 @@ export const tableDataFetch = data => {
       interceptorType: true, //当前页面是第二次进来的情况 && searchConditions 对象没有发生变化（用户没有操作分页或者更改任何查询条件）
     },
     data: data.query,
-    params: data.method === 'get'? data.query : data.params
+    params: data.method === 'get' ? data.query : data.params
   });
 };
 
