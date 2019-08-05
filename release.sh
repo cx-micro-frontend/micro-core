0223dfff016c98d03ecbc6387cf57f9963c48c35
xilanhuadechuntian <Lensgcx@163.com>
2019-08-05 11:44:43 +0800

    [release] Upgrade to 0.0.17-6

--- release.sh
+++ release.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+
+
+
+function handleFlow {
+
+    #build
+    if [ "$mode" = "custom" ]
+       then
+        npm version $V --no-git-tag-version
+       else
+        npm version prerelease --no-git-tag-version
+    fi
+
+    # commit
+    git tag "$V"
+    git commit -am "[release] Upgrade to $V"
+
+    # publish
+    npm info
+    npm publish --tag beta
+    echo Releasing pubish successful ...
+}
+
+
+
 set -e
 
 echo "commit and releasing pubish - are you sure? (y/n)"
@@ -7,18 +32,30 @@ read -p -n $REPLY
 
 if [[ $REPLY =~ ^[Yy]$ ]]
   then
+    echo "Do you want to enter version to releasing publish? (y/n)"
+    read -p -n $REPLY
+     if [[ $REPLY =~ ^[Yy]$ ]]
+     then
+        echo "Enter release version: "
+        read VERSION
 
-    # commit
-    git tag "v-%s"
-    git commit -am "[release] Upgrade to %s"
+        mode='custom'
+        V=$VERSION
 
-    # publish
-    npm version prerelease
-    npm info
-    npm publish --tag beta
-    echo Releasing pubish successful ...
+        handleFlow
+     else
+
+       mode='prerelease'
+       V="%s"
+
+       handleFlow
+     fi
+
 else
     echo Releasing pubish failed ...
 fi
 
 npm dist-tag ls
+
+
+
