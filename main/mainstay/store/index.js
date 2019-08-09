395fe572447301eba75a1cb477c998f82902b6a9
xilanhuadechuntian <Lensgcx@163.com>
2019-08-09 16:04:38 +0800

    Update index.js

--- main/mainstay/store/index.js
+++ main/mainstay/store/index.js
@@ -2,23 +2,30 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import getters from './getters';
 
+//core / mode
 import Core from './modules/System/Common/Core';
-
 import FrameMode from './modules/System/FrameMode/FrameMode';
 
+//common
+import SearchConditions from './modules/System/Common/SearchConditions';
+import RoleButton from './modules/System/Role/RoleButton';
+import RequestHead from './modules/System/Common/RequestHeader';
+import Table from './modules/System/Table/Table';
+
+
+//User
 import User from './modules/System/User/User';
 
+//Layout
+import CoverPainting from './modules/System/Layout/CoverPainting';
 import Sidebar from './modules/System/Layout/Sidebar';
 import PageInfo from './modules/System/Common/PageInfo';
 import PageTabs from './modules/System/Layout/PageTabs';
 import AsyncRouter from './modules/System/Router/AsyncRouter';
 
+//Tree
 import OrganizeTree from './modules/System/Tree/Organize-tree';
 
-import SearchConditions from './modules/System/Common/SearchConditions';
-import RoleButton from './modules/System/Role/RoleButton';
-import Table from './modules/System/Table/Table';
-import RequestHead from './modules/System/Common/RequestHeader';
 
 import entrance from '../../injection/entrance/entrance_core'
 
@@ -29,25 +36,29 @@ Vue.use(Vuex);
 const store = new Vuex.Store(
   Object.assign({}, entrance.model, {
       modules: {
-        Core,
 
+        //core
+        Core,
         FrameMode,
 
+        //common
+        Search: SearchConditions,
+        RoleButton,
+        RequestHead,
+        Table,
+
+        //User
         User,
 
+        //Layout
+        CoverPainting,
         Sidebar,
         PageInfo,
         PageTabs,
         AsyncRouter,
 
-
+        //Tree
         OrganizeTree,
-
-        Search: SearchConditions,
-        RoleButton,
-
-        Table,
-        RequestHead,
 
         AutoForm
       },
