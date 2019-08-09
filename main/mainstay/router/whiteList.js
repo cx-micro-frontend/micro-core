735682ffa0be7af503d800b9253e585f122c65da
xilanhuadechuntian <Lensgcx@163.com>
2019-08-09 12:05:48 +0800

    Update whiteList.js

--- main/mainstay/router/whiteList.js
+++ main/mainstay/router/whiteList.js
@@ -1,12 +1,19 @@
 /**
  * you can write path to set  no redirect whitelist
  */
-export default [
-  '/front',
+export default {
+  //无需权限登录的静态路由地址
+  normal: [
+    '/front',
 
-  '/front/login',
-  '/front/login-station',
+    '/front/login',
+    '/front/login-station',
 
-  '/404',
-  '/sso/404'
-];
+    '/404',
+    '/sso/404'
+  ],
+  //需要权限登录的静态路由地址
+  auth: [
+    '/dashboard'
+  ]
+}
