c92269ce4f27e41c1b567c318a24421faad9874f
xilanhuadechuntian <Lensgcx@163.com>
2019-08-15 16:29:08 +0800

    Create controller.js

---
+++ config/controller/controller.js
@@ -0,0 +1,58 @@
+module.exports = {
+  ['biz_components']: {
+    name: '业务组件',
+    useVuex: true,
+    useMixins: true,
+    useUI: true,
+    disabled: false, //整体禁用
+  },
+  ['system']: {
+    name: '系统设置',
+    useVuex: true,
+    useMixins: true,
+    useUI: true,
+    disabled: false, //整体禁用
+  },
+  ['community']: {
+    name: '阳光城公告',
+    useVuex: true,
+    useMixins: true,
+    useUI: true,
+    disabled: false, //整体禁用
+  },
+  ['owner']: {
+    name: '业户',
+    useVuex: true,
+    useMixins: true,
+    useUI: true,
+    disabled: false, //整体禁用
+  },
+  ['detailcheck']: {
+    name: '细部检查',
+    useVuex: true,
+    useMixins: true,
+    useUI: true,
+    disabled: false, //整体禁用
+  },
+  ['deilver']: {
+    name: '交付',
+    useVuex: true,
+    useMixins: true,
+    useUI: true,
+    disabled: false, //整体禁用
+  },
+  ['focusdeilver']: {
+    name: '集中交付',
+    useVuex: true,
+    useMixins: true,
+    useUI: true,
+    disabled: false, //整体禁用
+  },
+  ['checkhousepara']: {
+    name: '验房参数',
+    useVuex: true,
+    useMixins: true,
+    useUI: true,
+    disabled: false, //整体禁用
+  },
+};
