4d36e3cbc3de638ae9ca41589489f3f822028b70
xilanhuadechuntian <Lensgcx@163.com>
2019-08-22 15:19:36 +0800

    Update RequestHeader.js

--- main/src/store/modules/System/Common/RequestHeader.js
+++ main/src/store/modules/System/Common/RequestHeader.js
@@ -6,6 +6,22 @@ const requestHead = {
       appId: '07d8737811434732',
       appClientType: 'pc',
       'Set-Cookie': getToken(),
+      funcId: null,
+    },
+  },
+  mutations: {
+    SET_FUNCID_TO_HEAD: (state, data) => {
+      console.log(12313213123);
+      console.log(12313213123);
+      console.log(data);
+      console.log(12313213123);
+      console.log(12313213123);
+      state.base.funcId = data;
+    },
+  },
+  actions: {
+    setFuncIdToHead: ({ commit }, data) => {
+      commit('SET_FUNCID_TO_HEAD', data);
     },
   },
 };
