3c2a78963df86be180db41e00f50e58e231de828
xilanhuadechuntian <Lensgcx@163.com>
2019-08-22 15:19:33 +0800

    Update whiteList.js

--- main/src/router/whiteList.js
+++ main/src/router/whiteList.js
@@ -3,7 +3,15 @@
  */
 export default {
   //无需权限登录的静态路由地址
-  normal: ['/front', '/front/login', '/front/login-station', '/404', '/sso/404'],
+  normal: [
+    '/front',
+    '/front/login',
+    '/front/login-station',
+    '/404',
+    '/sso/404',
+    '/sso/usererror',
+    '/sso/norouterole',
+  ],
   //需要权限登录的静态路由地址
   auth: ['/overview'],
 };
