3e0317e51f979aed89f6c8830dd2ba4aa97cdabd
xilanhuadechuntian <Lensgcx@163.com>
2019-08-28 15:20:12 +0800

    Create error-page.js

---
+++ main/src/locales/zh-CN/error-page.js
@@ -0,0 +1,19 @@
+export default {
+  'normal.error_route_role.main': '您要访问的模块页面走丢啦',
+  'normal.error_route_role.sub': '请联系管理员',
+
+  'normal.error_no_pages.main': '您要访问的模块页面走丢啦',
+  'normal.error_no_pages.sub': '请联系管理员',
+
+  'sso.error_login.main': '单点登录错误',
+  'sso.error_login.sub': '请与账号管理员联系',
+
+  'sso.error_token.main': '单点登录用户不存在',
+  'sso.error_token.sub': '请与账号管理员联系',
+
+  'sso.error_route_role.main': '该用户路由权限缺失',
+  'sso.error_route_role.sub': '请与账号管理员联系',
+
+  'sso.error_no_pages.main': '您要访问的模块页面不存在',
+  'sso.error_no_pages.sub': '请联系管理员',
+};
