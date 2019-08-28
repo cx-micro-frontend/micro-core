025230d0a87670331b780986c10374ad476359d5
xilanhuadechuntian <Lensgcx@163.com>
2019-08-28 15:19:31 +0800

    Update index.js

--- clone/index.js
+++ clone/index.js
@@ -11,6 +11,8 @@ const controller = require(path.resolve(__dirname, '../config/controller/control
 
 const clone = require('./clone');
 const injection = require('./injection');
+const filesMap = require('./filesMap');
+
 
 /**
  * create dir
@@ -49,12 +51,15 @@ injection.injection_core_entry(injectList);
 signale.start('Start injecting UI entry ...\n');
 injection.injection_ui_entry(injectList);
 
+//Create route files map by repositories
+signale.start('Start create route files map list ...\n');
+//create file in injection => config dir
+utils.judgeAndMkdir(utils.inJectPath().config);
+filesMap.createRouteFiles();
+
 //clone static dir
 signale.start('Start copy static dir ...\n');
 clone.cloneStatic();
-
-
-
 
 
 
