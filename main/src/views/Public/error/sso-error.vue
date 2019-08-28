40c6fc7c4db9c90534110ef87e25d056c9d3ee68
xilanhuadechuntian <Lensgcx@163.com>
2019-08-28 15:21:26 +0800

    Create sso-error.vue

---
+++ main/src/views/Public/error/sso-error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="error-page sso-error-page">
+    <img :src="img_page_error"/>
+    <div class="error-text">
+      <p>{{maintit}}</p>
+      <p>{{subtit}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import mixins from './mixins'
+
+  export default {
+    mixins: [mixins],
+  };
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "../error-page";
+</style>
