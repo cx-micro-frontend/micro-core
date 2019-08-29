db4554201a6d4274809f1d6b11c7651f2aa0a8c0
xilanhuadechuntian <Lensgcx@163.com>
2019-08-29 15:07:42 +0800

    Update filesMap.js

--- clone/filesMap.js
+++ clone/filesMap.js
@@ -6,55 +6,63 @@ const fs = require('fs');
 const join = require('path').join;
 const utils = require('./utils');
 
-const getRouteFiles = () => {
+//modules config by env.param.config file
+const config = require(path.resolve('env.param.config'));
+const modulesConfig = config.prod_injection.modules || [];
 
+const getRouteFiles = () => {
   let routeFiles = [];
 
-  //clone 仓库路径
-  const repositoriesPath = path.resolve(__dirname, '../injection/repositories');
+  modulesConfig.forEach(module => {
+    if (module.disabled) return;
 
-  //获取仓库目录下模块列表
-  const repositoriesFiles = fs.readdirSync(repositoriesPath);
+    const repositoryName = utils.repositoryName(module);
 
-  //循环模块列表
-  repositoriesFiles.forEach((item, index) => {
-    //到views目录下
-    let viewsFilesPath = join(repositoriesPath, `${item}/views`);
-    //判断是否存在,提出一些组件库模块，它们没有页面，无需注入
+    /*
+     * The current module :
+     * its own module => in root path
+     * not its own module => in neap => injection => repositories
+     */
+    const repositoryPath = module.isOwner
+      ? join(utils.inJectPath().root, `${repositoryName}`)
+      : join(path.resolve(__dirname, '../injection/repositories'), `${repositoryName}`);
+
+    // views dir path
+    let viewsFilesPath = join(repositoryPath, 'views');
+
+    /*
+     * To judge whether views dir exists,
+     * every buiness modules has its views dir
+     * propose some component library modules, which have no pages and need not be injected.
+     */
     if (fs.existsSync(viewsFilesPath)) {
+      //Locate in the target directory for processing
+      const viewsFiles = fs.readdirSync(viewsFilesPath); //获取仓库目录下views文件夹下，所有文件列表
 
-      const viewsFiles = fs.readdirSync(viewsFilesPath);//获取仓库目录下views文件夹下，所有文件列表
-
-      // routeFiles.push('=================')
-      //定位到目标目录下进行处理
-
-      //循环一级页面目录
+      //Loop firest level page directory
       viewsFiles.forEach(firstDir => {
-        let route1_Path = join(viewsFilesPath, firstDir);//一级模块路径
-        let stat1 = fs.statSync(route1_Path);//一级模块信息
+        let route1_Path = join(viewsFilesPath, firstDir); //一级模块路径
+        let stat1 = fs.statSync(route1_Path); //一级模块信息
         if (stat1.isDirectory() === true) {
-
           routeFiles.push({
-            sign: `===== ${firstDir} module routing allowance list =====`
+            sign: `===== ${firstDir} module routing allowance list =====`,
           });
 
-          //循环二级页面目录
+          //Loop second level page directory
           fs.readdirSync(route1_Path).forEach(secondDir => {
-            let route2_Path = join(route1_Path, secondDir);//二级模块路径
-            let stat2 = fs.statSync(route2_Path);//二级模块信息
+            let route2_Path = join(route1_Path, secondDir); //二级模块路径
+            let stat2 = fs.statSync(route2_Path); //二级模块信息
             if (stat2.isDirectory() === true) {
-
               /*
                * 判断该二级模块下是否存在同名.vue文件
                * 例如： （firstModule => secondModule => secondModule.vue)
                */
               if (fs.existsSync(join(route2_Path, `${secondDir}.vue`))) {
-                routeFiles.push(`/${firstDir}/${secondDir}`);//拼接注入数组队列
+                routeFiles.push(`/${firstDir}/${secondDir}`); //拼接注入数组队列
               }
             }
           });
         }
-
       });
     }
   });
@@ -62,10 +70,7 @@ const getRouteFiles = () => {
   return routeFiles;
 };
 
-
 exports.createRouteFiles = () => {
-
-
   const tips = type => `/* eslint-disable */
 /*
  * Create route files map by repositories
@@ -86,15 +91,13 @@ exports.createRouteFiles = () => {
     return getRouteFiles().map(route => {
       if (typeof route === 'string') {
         return `  '${route}',`;
-      }
-      else {
+      } else {
         return `  
   //${route.sign}
   `;
       }
-    })
+    });
   }
-
 
   const content = `${tips('core')}
 export default [
