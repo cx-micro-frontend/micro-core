9eb99675ef7cd4124337607139add081eed3f63d
xilanhuadechuntian <Lensgcx@163.com>
2019-08-29 15:07:50 +0800

    Update utils.js

--- clone/utils.js
+++ clone/utils.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const shell = require('shelljs');
 const packageJson = require(path.resolve('./package.json'));
 
-
 /**
  * inJect path
  * @returns {{injection: string, entrance: string, core: string, repositorie: string, repositorie_tmp: string}}
@@ -12,34 +11,31 @@ const packageJson = require(path.resolve('./package.json'));
 exports.inJectPath = () => {
   const injection = path.resolve(__dirname, '../injection');
   return {
+    root: path.resolve(), //root
 
-    root: path.resolve(),//root
+    injection: injection, //injection dir
 
-    injection: injection,//injection dir
+    entrance: `${injection}/entrance`, //injection entrance dir
 
-    entrance: `${injection}/entrance`,//injection entrance dir
+    repositorie: `${injection}/repositories`, //repositorie dir
 
-    repositorie: `${injection}/repositories`,//repositorie dir
+    config: `${injection}/config`, //config dir
 
-    config: `${injection}/config`,//config dir
+    core: `${injection}/entrance/entrance_core.js`, //core js-file for injection entrance
 
-    core: `${injection}/entrance/entrance_core.js`,//core js-file for injection entrance
+    ui: `${injection}/entrance/entrance_ui.js`, //core js-file for injection entrance
 
-    ui: `${injection}/entrance/entrance_ui.js`,//core js-file for injection entrance
+    repositorie_tmp: `${injection}/repositories/_tmp`, //repositorie tmp
 
-    repositorie_tmp: `${injection}/repositories/_tmp`,//repositorie tmp
+    routefiles: `${injection}/config/routefiles.js`, //route files
 
-    routefiles: `${injection}/config/routefiles.js`,//route files
-
-    static: path.resolve(__dirname, '../lib/static'),//static file
-
-  }
+    static: path.resolve(__dirname, '../lib/static'), //static file
+  };
 };
 
 exports.version = () => {
   return process.env.VERSION || packageJson.version;
 };
-
 
 exports.judgeAndMkdir = (path, cb = null) => {
   if (fs.existsSync(path)) {
@@ -51,33 +47,37 @@ exports.judgeAndMkdir = (path, cb = null) => {
   cb();
 };
 
-
 exports.moduleRename = (name, type) => {
   return 'inject' + '_' + name + '__' + type;
 };
 
 exports.repositoryName = module => {
-
   const namespace = 'NS';
 
   return `${namespace}_${module.repositorie}`;
 };
 
-
-exports.NumbertoLen2String = (n) => {
+exports.NumbertoLen2String = n => {
   if (n.toString().length === 1) {
     return '0' + n.toString();
-  }
-  else {
+  } else {
     return n;
   }
 };
 
 exports.sysdate = () => {
   const myDate = new Date();
-  return myDate.getFullYear() + "-" + this.NumbertoLen2String(Number(myDate.getMonth()) + Number(1)) + "-" + this.NumbertoLen2String(myDate.getDate())
-    + " " + this.NumbertoLen2String(myDate.getHours()) + ":" + this.NumbertoLen2String(myDate.getMinutes()) + ":" + this.NumbertoLen2String(myDate.getSeconds())
+  return (
+    myDate.getFullYear() +
+    '-' +
+    this.NumbertoLen2String(Number(myDate.getMonth()) + Number(1)) +
+    '-' +
+    this.NumbertoLen2String(myDate.getDate()) +
+    ' ' +
+    this.NumbertoLen2String(myDate.getHours()) +
+    ':' +
+    this.NumbertoLen2String(myDate.getMinutes()) +
+    ':' +
+    this.NumbertoLen2String(myDate.getSeconds())
+  );
 };
-
-
-
