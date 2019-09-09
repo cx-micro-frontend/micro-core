27f95615a2e30d991ca010397cc86042d8f3b6a0
xilanhuadechuntian <Lensgcx@163.com>
2019-09-09 10:23:57 +0800

    Update controller.js

--- config/controller/controller.js
+++ config/controller/controller.js
@@ -1,9 +1,9 @@
 module.exports = {
   ['biz_components']: {
     name: '业务组件',
-    useVuex: true,
-    useMixins: true,
-    useUI: true,
+    useVuex: true, //是否使用注入 Vuex
+    useMixins: true, //是否使用注入 Mixins
+    useUI: true, //是否使用注入 UI
     disabled: false, //整体禁用
   },
   ['system']: {
@@ -11,55 +11,55 @@ module.exports = {
     useVuex: true,
     useMixins: true,
     useUI: true,
-    disabled: false, //整体禁用
+    disabled: false,
   },
   ['community']: {
     name: '阳光城公告',
     useVuex: true,
     useMixins: true,
     useUI: true,
-    disabled: false, //整体禁用
+    disabled: false,
   },
   ['customer']: {
     name: '客户审核',
     useVuex: true,
     useMixins: true,
     useUI: true,
-    disabled: false, //整体禁用
+    disabled: false,
   },
   ['owner']: {
     name: '业户',
     useVuex: true,
     useMixins: true,
     useUI: true,
-    disabled: false, //整体禁用
+    disabled: false,
   },
   ['detailcheck']: {
     name: '细部检查',
     useVuex: true,
     useMixins: true,
     useUI: true,
-    disabled: false, //整体禁用
+    disabled: false,
   },
   ['deilver']: {
     name: '交付',
     useVuex: true,
     useMixins: true,
     useUI: true,
-    disabled: false, //整体禁用
+    disabled: false,
   },
   ['focusdeilver']: {
     name: '集中交付',
     useVuex: true,
     useMixins: true,
     useUI: true,
-    disabled: false, //整体禁用
+    disabled: false,
   },
   ['checkhousepara']: {
     name: '验房参数',
     useVuex: true,
     useMixins: true,
     useUI: true,
-    disabled: false, //整体禁用
+    disabled: false,
   },
 };
