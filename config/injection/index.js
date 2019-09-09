ec0b118c7586775f2b31f3de2ebc847b9c90e8fd
xilanhuadechuntian <Lensgcx@163.com>
2019-09-09 10:23:53 +0800

    Update index.js

--- config/injection/index.js
+++ config/injection/index.js
@@ -15,18 +15,19 @@ const VarInjection = require(`./version/${prefix}${version}`);
 
 const _BasicInjection = [
   {
-    module: '业务组件',
-    path: 'http://192.168.1.47:3000/Framework-frontend/micro_biz_components.git',
-    repositorie: 'biz_components',
-    isOwner: false,
-    disabled: false,
+    module: '业务组件', //模块中文名称
+    repositorie: 'biz_components', //模块唯一标识key
+    path: 'http://192.168.1.47:3000/Framework-frontend/micro_biz_components.git', //clone 路径
+    isOwner: false, //是否为自有模块
+    disabled: false, //是否禁用
   },
   {
-    module: '系统设置',
-    path: 'http://192.168.1.47:3000/Framework-frontend/micro_system.git',
-    repositorie: 'system',
-    isOwner: false,
-    disabled: false,
+    module: '系统设置', //模块中文名称
+    repositorie: 'system', //模块唯一标识key
+    path: 'http://192.168.1.47:3000/Framework-frontend/micro_system.git', //clone 路径
+    landingRoot: true, //是否注入根目录中
+    isOwner: false, //是否为自有模块
+    disabled: false, //是否禁用
   },
 ];
 
