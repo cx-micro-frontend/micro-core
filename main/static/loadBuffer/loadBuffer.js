2509d36498a4ed5c7e4645686aa17f8951331404
xilanhuadechuntian <Lensgcx@163.com>
2019-09-25 10:01:07 +0800

    [0.0.23-95] Upgrade to V-0.0.23-95

--- main/static/loadBuffer/loadBuffer.js
+++ main/static/loadBuffer/loadBuffer.js
@@ -4,6 +4,7 @@
 function createLoading() {
   var loadingDom = document.createElement('img');
   loadingDom.setAttribute('src', './static/loadBuffer/loading.gif');
+  loadingDom.setAttribute('id', 'loadingBuffer');
   loadingDom.style.cssText =
     'position: absolute;z-index: 1; width: 80px;height: 80px;top: 50%;left: 50%;margin: -80px 0 0 -40px';
   document.body.appendChild(loadingDom);
@@ -12,6 +13,6 @@ function createLoading() {
 //init
 createLoading();
 
-document.ready = function() {
+window.onload = function() {
   document.body.removeChild(document.getElementById('loadingBuffer'));
 };
