606da272ac71ffb543b106ddc8a982a47a9a3d14
xilanhuadechuntian <Lensgcx@163.com>
2019-10-15 16:02:46 +0800

    Update NS-biz-user-dropdown.vue

--- main/src/components/NS-biz-user-dropdown/NS-biz-user-dropdown.vue
+++ main/src/components/NS-biz-user-dropdown/NS-biz-user-dropdown.vue
@@ -1,6 +1,7 @@
 <!--用户下拉菜单-->
 <template>
   <div class="biz-user-dropdown">
+
     <ns-user-dropdown
       :options="options"
       :avator="avatar"
@@ -12,6 +13,9 @@
     </ns-user-dropdown>
 
     <About :visible="aboutVisible"></About>
+
+    <!--个人设置弹窗-->
+    <personal-setting-dialog :visible.sync="safeSettingVisible"></personal-setting-dialog>
   </div>
 </template>
 
@@ -19,22 +23,25 @@
 
   import { mapGetters } from 'vuex';
   import About from './About/About';
-
+  import personalSettingDialog from './UserSetting/UserSettingDialog';
 
   export default {
     name: 'biz-user-dropdown',
-    components: { About },
+    components: { About, personalSettingDialog },
     data() {
       return {
         abbreviation: true,//头像为简单模式 - 显示用户姓名的最后一个字
+
         options: [
-          { label: '修改密码', value: 'editPassword' },
+          { label: '个人设置', value: 'personalSetting' },
           { label: '关于', value: 'about' },
           { label: '退出登录', value: 'logout' },
         ],
         aboutVisible: {
           visible: false,
         },
+
+        safeSettingVisible: false,
       };
     },
     computed: {
@@ -43,8 +50,9 @@
     methods: {
       userDropdownClick(value, index) {
         switch (value) {
-          case  'editPassword':
-            this.editPassword();
+          //个人设置
+          case  'personalSetting':
+            this.safeSettingVisible = true;
             break;
           case  'about':
             this.aboutVisible.visible = true;
@@ -62,9 +70,6 @@
       logout() {
         // login out,empty store and cookie by fun of 'loginout'
         this.$store.dispatch('logOut');
-      },
-      editPassword() {
-
       },
     },
   };
