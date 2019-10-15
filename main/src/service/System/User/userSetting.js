e6ba88681f287de156743cf567ce4237215e0867
xilanhuadechuntian <Lensgcx@163.com>
2019-10-15 16:03:34 +0800

    Create userSetting.js

---
+++ main/src/service/System/User/userSetting.js
@@ -0,0 +1,86 @@
+import { dataFilter } from '@NEAP/utils/fetch/fetchDataType';
+import fetch from '@NEAP/utils/fetch/fetch';
+import qs from 'querystring';
+
+/**
+ * 获取员工的基本信息
+ * @param param
+ */
+export function getUserInfo(param) {
+  //Filter data
+  dataFilter(param);
+  //fetch out
+  return fetch({
+    url: '/system/lcinfo/user-info-by-account',
+    method: 'get',
+    params: param,
+  });
+}
+
+/**
+ * 更新密码
+ * @param param
+ */
+export function updatePassword(param) {
+  //Filter data
+  dataFilter(param);
+  //fetch out
+  return fetch({
+    url: '/oauth/oauth/update-password',
+    method: 'post',
+    data: param,
+    transformRequest: [
+      function(data) {
+        data = qs.stringify(data);
+        return data;
+      },
+    ],
+  });
+}
+
+/**
+ * 编辑活动
+ * @param param
+ */
+export function editActivity(param) {
+  //Filter data
+  dataFilter(param);
+  //fetch out
+  return fetch({
+    url: '/property-service/property-rest/rest/notice/edit',
+    method: 'post',
+    data: param,
+    transformRequest: transformRequest,
+  });
+}
+
+/**
+ * 编辑活动
+ * @param param
+ */
+export function delActivity(param) {
+  //Filter data
+  dataFilter(param);
+  //fetch out
+  return fetch({
+    url: '/property-service/property-rest/rest/notice/delete',
+    method: 'get',
+    params: param,
+  });
+}
+
+/**
+ * 发送短信
+ * @param param
+ */
+export function sendMessage(param) {
+  //Filter data
+  dataFilter(param);
+  //fetch out
+  return fetch({
+    url: '/property-service/property-rest/rest/notice/resendApplyCode',
+    method: 'post',
+    params: param,
+    transformRequest: transformRequest,
+  });
+}
