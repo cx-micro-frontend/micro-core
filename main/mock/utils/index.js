53f9346de2f4c9a744aa18e7ce10faee7ab90d36
xilanhuadechuntian <Lensgcx@163.com>
2019-10-18 16:34:57 +0800

    Create index.js

---
+++ main/mock/utils/index.js
@@ -0,0 +1,31 @@
+/**
+ * get query
+ * @param url
+ * @param key
+ * @returns {null}
+ */
+export function getQuery(url, key) {
+  let reg = new RegExp('(^|\\?|&)' + key + '=([^&]*)(\\area-linkage|&|$)', 'i');
+  if (url) {
+    if (reg.test(url)) {
+      const val = unescape(RegExp.$2.replace(/\+/g, ' '));
+
+      if (val !== null && val.toString().length >= 1) {
+        return val;
+      }
+    }
+  }
+  return null;
+}
+
+/**
+ * get mock query
+ * @param url
+ * @param key
+ * @returns {string}
+ */
+export const getMockQuery = (url, key) => {
+  const URL = encodeURI(url);
+  const qn = getQuery(URL, key);
+  return decodeURI(decodeURI(qn));
+};
