a9dc44bf82b12cddcfe2b4afbe5793aa61dcdbe9
xilanhuadechuntian <Lensgcx@163.com>
2019-10-21 10:15:18 +0800

    Create LockScreen.js

---
+++ main/src/store/modules/System/Layout/LockScreen.js
@@ -0,0 +1,33 @@
+import { storageHandle } from '../../../../utils/storage/storage';
+
+const LockScreen = {
+  state: {
+    isLocked: JSON.parse(storageHandle('get', 'sign_isLocked')) || 12312,
+  },
+  mutations: {
+    LOCK_SCREEN: state => {
+      state.isLocked = true;
+      storageHandle('set', 'sign_isLocked', state.isLocked);
+    },
+    UN_LOCK_SCREEN: state => {
+      state.isLocked = false;
+      storageHandle('set', 'sign_isLocked', state.isLocked);
+    },
+    REMOVE_LOCK_SCREEN: state => {
+      state.isLocked = false;
+      storageHandle('remove', 'sign_isLocked');
+    },
+  },
+  actions: {
+    lockScreen({ commit }) {
+      commit('LOCK_SCREEN');
+    },
+    unLockScreen({ commit }) {
+      commit('UN_LOCK_SCREEN');
+    },
+    removeLockScreen({ commit }) {
+      commit('REMOVE_LOCK_SCREEN');
+    },
+  },
+};
+export default LockScreen;
