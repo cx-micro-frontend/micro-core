523a2065fb886ae9cf2a5cba952b91f9995fca19
xilanhuadechuntian <Lensgcx@163.com>
2019-11-15 14:49:32 +0800

    Create index.js

---
+++ main/src/UI/Element-UI/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+
+//base style
+import 'cx-theme/dist/cx-theme.min.css';
+//cover style
+import '../../style/element-cover/index.scss';
+
+Vue.use(ElementUI);
