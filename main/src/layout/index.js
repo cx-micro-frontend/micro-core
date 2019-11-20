6cfc7cbc477073a0ce3216d178891aba8da370dc
xilanhuadechuntian <Lensgcx@163.com>
2019-11-20 15:41:50 +0800

    Update index.js

--- main/src/layout/index.js
+++ main/src/layout/index.js
@@ -2,7 +2,5 @@ export { default as bizSidebar } from '../components/NS-biz-sidebar/biz-sidebar'
 export { default as bizTabsViews } from '../components/NS-biz-page-tabs/NS-biz-page-tabs';
 export { default as bizLockScreen } from '../components/NS-biz-lockscreen/NS-biz-lockscreen';
 export { default as bizSkiner } from '../components/NS-biz-skiner/Biz-skiner';
-export {
-  default as bizUserDropdown,
-} from '../components/NS-biz-user-dropdown/NS-biz-user-dropdown';
+export { default as bizUserDropdown } from '../components/NS-biz-user-dropdown/NS-biz-user-dropdown';
 export { default as headerCustom } from './header-custom';
