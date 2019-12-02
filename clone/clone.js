c6eca54be0bd84752152e96b4b09f1510e850079
xilanhuadechuntian <Lensgcx@163.com>
2019-12-02 14:05:02 +0800

    Update clone.js

--- clone/clone.js
+++ clone/clone.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const os = require('os');
 const shell = require('shelljs');
 const utils = require('./utils');
-
+const copy_rename = require('./config').copy_rename;
 const osType = os.type(); // system type
 
 exports.cloneRepositories = () => {
@@ -62,36 +62,83 @@ exports.cloneRepositories = () => {
 };
 
 /**
- * copy static
+ * copy and inject static dir
  */
 exports.cloneStatic = () => {
   // shell.rm('-rf', `${utils.inJectPath().root}/static`);
 
-  const static_inJect = `${utils.inJectPath().static}`; //inJect static path
-  const static_root = `${utils.inJectPath().root}/static`; //root static path
+  const static_inJect_path = `${utils.inJectPath().static}`; //inJect static path
+  const static_target_path = `${utils.inJectPath().root}/static`; //root static path
 
   /*
    * judge static in root is exists or not:
-   * yes = > remove and create dir
    * not => create dir
+   * 目标目录下没有 static 文件就为其创建一个
    */
-  utils.judgeAndMkdir(static_root);
+  utils.mkdir(static_target_path);
 
   //judge static dir in neap-injection
-  if (fs.existsSync(static_inJect)) {
+  if (fs.existsSync(static_inJect_path)) {
     const whitestatic = ['.gitkeep', 'loadBuffer', 'intercept'];
+
     const staticInclude = utils.getInjectConfig().staticInclude || [];
 
     //Locate in the target directory for processing
-    const static_inJect_files = fs.readdirSync(static_inJect); //get all files/dirs in inJect static path
+    const static_inJect_files = fs.readdirSync(static_inJect_path); //get all files/dirs in inJect static path
+
+    const NEAP_static_target_path = `${static_target_path}/${copy_rename.static}`;
+
+    /**
+     * judge exists:
+     * yes = > remove
+     */
+    utils.judgeAndMkdir(NEAP_static_target_path);
+
+    console.log(22222222222222);
+    console.log(static_inJect_files);
+    console.log(NEAP_static_target_path);
+    console.log(2222222222222222);
 
     //Loop firest level page directory
     static_inJect_files.forEach(file => {
-      const file_Path = join(static_inJect, file); //file path
+      const file_Path = join(static_inJect_path, file); //file path
 
       if ([...whitestatic, ...staticInclude].indexOf(file) > -1) {
-        shell.cp('-R', file_Path, static_root);
+        shell.cp('-R', file_Path, NEAP_static_target_path);
       }
     });
+  }
+};
+
+/**
+ * copy and inject mock dir
+ */
+exports.cloneMock = () => {
+  const mock_inJect_path = `${utils.inJectPath().mock}`; //获取待注入的mock路径
+
+  const mock_target_path = utils.getMockPath(); //获取注入路径
+
+  /**
+   * judge mock dir in neap-injection
+   * 判断当前执行环节目录下，母体中是否存在 mock 文件，存在，则进行拷贝注入操作，否则无需处理
+   */
+  if (fs.existsSync(mock_inJect_path)) {
+    //目标目录下没有mock文件就为其创建一个
+    utils.mkdir(mock_target_path);
+
+    const NEAP_mock_target_path = `${mock_target_path}/${copy_rename.mock}`;
+
+    /**
+     * judge exists:
+     * yes = > remove
+     */
+    utils.judgeAndRemove(NEAP_mock_target_path);
+
+    console.log(333333333333333);
+    console.log(mock_inJect_path);
+    console.log(NEAP_mock_target_path);
+    console.log(333333333333333);
+
+    shell.cp('-R', mock_inJect_path, NEAP_mock_target_path);
   }
 };
