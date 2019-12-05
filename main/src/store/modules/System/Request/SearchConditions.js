951a0ad071d93c5edde2a9a0c71b0ca24f0a8927
xilanhuadechuntian <Lensgcx@163.com>
2019-12-05 14:35:24 +0800

    Create SearchConditions.js

---
+++ main/src/store/modules/System/Request/SearchConditions.js
@@ -0,0 +1,51 @@
+const baseConditions = {
+  companyId: '', //公司id
+  departmentId: '', //部门id
+  organizationId: '',
+  dictionaryitemDictionaryId: '',
+  filterList: [], //条件
+  pageNum: 1, //当前页数
+  pageSize: 10, //每页显示条目个数
+  orderBy: '', //排序：升序还是降序
+  orderFieldName: '', //排序：字段名
+  mainSearch: '', //输入框值
+  filterConditions: [], //筛选器记录的条件
+  otherConditions: {},
+  size: '',
+  showChildren: false, //是否包含子孙节点查询
+};
+
+const SearchConditions = {
+  state: {
+    //search criteria
+    conditions: {},
+  },
+  mutations: {
+    SET_SEARCG_CONDITIONS: (state, ID) => {
+      if (!ID) {
+        return false;
+      }
+      if (state.conditions.hasOwnProperty(ID)) {
+        return false;
+      }
+      state.conditions[ID] = JSON.parse(JSON.stringify(baseConditions));
+    },
+
+    EMPTY_SEARCH_CONDITIONS: (state, ID) => {
+      console.log(ID, 'empty');
+      if (state.conditions[ID]) {
+        delete state.conditions[ID];
+      }
+    },
+  },
+  actions: {
+    setSearchConditions({ commit }, ID) {
+      commit('SET_SEARCG_CONDITIONS', ID);
+    },
+    emptySearchConditions({ commit }, ID) {
+      commit('EMPTY_SEARCH_CONDITIONS', ID);
+    },
+  },
+};
+
+export default SearchConditions;
