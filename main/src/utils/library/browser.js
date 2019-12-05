537834479b61277cba37ea7a3b08e3dd61f2d043
xilanhuadechuntian <Lensgcx@163.com>
2019-12-05 14:35:04 +0800

    Update browser.js

--- main/src/utils/library/browser.js
+++ main/src/utils/library/browser.js
@@ -1,16 +1,29 @@
+/**
+ * 获取浏览器信息
+ */
 export const getBrowserInfo = () => {
-  const userAgent = navigator.userAgent;
-  const info = {};
+  let info = {};
+  const ua = navigator.userAgent.toLowerCase();
 
-  if (userAgent.indexOf('Trident') > -1) {
-    info.browser = 'IE';
+  let i;
+  (i = ua.match(/rv:([\d.]+)\) like gecko/))
+    ? (info = { browser: 'ie', version: i[1] })
+    : (i = ua.match(/msie ([\d\.]+)/))
+    ? (info = { browser: 'ie', version: i[1] })
+    : (i = ua.match(/edge\/([\d\.]+)/))
+    ? (info = { browser: 'edge', version: i[1] })
+    : (i = ua.match(/firefox\/([\d\.]+)/))
+    ? (info = { browser: 'firefox', version: i[1] })
+    : (i = ua.match(/(?:opera|opr).([\d\.]+)/))
+    ? (info = { browser: 'opera', version: i[1] })
+    : (i = ua.match(/chrome\/([\d\.]+)/))
+    ? (info = { browser: 'chrome', version: i[1] })
+    : (i = ua.match(/version\/([\d\.]+).*safari/))
+    ? (info = { browser: 'safari', version: i[1] })
+    : 0;
 
-    info.ieVer =
-      userAgent.indexOf('rv:11.0') > -1
-        ? 11
-        : parseInt(userAgent.split(';')[1].replace(' MSIE ', ''));
-  } else if (userAgent.indexOf('Chrome') > -1) {
-    info.browser = 'Chrome';
-  }
+  info.browser = info.browser || 'unkonwn';
+  info.version = info.version || 'unkonwn';
+
   return info;
 };
