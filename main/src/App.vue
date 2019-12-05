09313231ea3a1a55a456990335d29828354cd97e
xilanhuadechuntian <Lensgcx@163.com>
2019-12-05 14:33:57 +0800

    Update App.vue

--- main/src/App.vue
+++ main/src/App.vue
@@ -7,6 +7,10 @@
 <script>
   export default {
     name: 'app',
+    created() {
+      this.$store.dispatch('setBrowserInfo');//设置浏览器信息
+      this.$store.dispatch('setOSInfo');//设置操作系统信息
+    },
   };
 </script>
 
