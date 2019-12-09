aee60d974c096cd431ac52f12c8895af4fe105a5
xilanhuadechuntian <Lensgcx@163.com>
2019-12-09 14:40:30 +0800

    Update clone.sh

--- injecter/clone.sh
+++ injecter/clone.sh
@@ -2,10 +2,12 @@
 
 # 模块名称
 MODULE_NAME=$1
+# 分支名称
+BRANCH_NAME=$2
 # 仓库拷贝地址
-CLONE_DRESS=$2
+CLONE_DRESS=$3
 # 本地复制路径
-CLONE_ADDRESS=$3
+CLONE_ADDRESS=$4
 
 # clone 开始时间
 SHOW_TIME=`date +%s`
@@ -14,7 +16,7 @@ SHOW_TIME=`date +%s`
 # git clone
 echo
 echo "cloning target:" ${MODULE_NAME}""
-git clone $CLONE_DRESS $CLONE_ADDRESS
+git clone -b $BRANCH_NAME $CLONE_DRESS $CLONE_ADDRESS
 # 结束时间
 END_TIME=`date +%s`
 
