68eab8a20c8c0253459fffe31afc953006a572fa
xilanhuadechuntian <Lensgcx@163.com>
2019-12-09 14:40:35 +0800

    Update micro_core_0.0.1.js

--- config/injection/version/micro_core_0.0.1.js
+++ config/injection/version/micro_core_0.0.1.js
@@ -1,102 +1,97 @@
 module.exports = [
-  //o2o - community
-  //   {
-  //     module: '阳光城公告',
-  //     path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o-sunshineCity.git',
-  //     repositorie: 'community',
-  //     isOwner: false,
-  //     disabled: false,
-  //   },
+  // //o2o - community
+  // {
+  //   module: '阳光城公告',
+  //   repositorie: 'community',
+  //   path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o-sunshineCity.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
   //
-  //   //o2o - admin
-  //   {
-  //     module: '客户审核',
-  //     path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o_admin.git',
-  //     repositorie: 'customer', //o2o-admin
-  //     isOwner: false,
-  //     disabled: false,
-  //   },
-  //   {
-  //     module: '客户审核',
-  //     path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o_admin.git',
-  //     repositorie: 'repairMessage', //o2o-admin
-  //     isOwner: false,
-  //     disabled: false,
-  //   },
-  //   {
-  //     module: '频道管理',
-  //     path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o_admin.git',
-  //     repositorie: 'message', //o2o-admin
-  //     isOwner: false,
-  //     disabled: false,
-  //   },
-  //   {
-  //     module: '系统运维',
-  //     path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o_admin.git',
-  //     repositorie: 'maintenance', //o2o-admin
-  //     isOwner: false,
-  //     disabled: false,
-  //   },
+  // //o2o - admin
+  // {
+  //   module: '客户审核',
+  //   repositorie: 'customer', //o2o-admin
+  //   path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o_admin.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '客户审核',
+  //   repositorie: 'repairMessage', //o2o-admin
+  //   path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o_admin.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '频道管理',
+  //   repositorie: 'message', //o2o-admin
+  //   path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o_admin.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '系统运维',
+  //   repositorie: 'maintenance', //o2o-admin
+  //   path: 'http://192.168.1.47:3000/O2O_frontend/micro_o2o_admin.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
   //
-  //   // //业户
-  //   // {
-  //   //   module: '业户',
-  //   //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
-  //   //   repositorie: 'owner',
-  //   //   isOwner: false,
-  //   //   disabled: false,
-  //   // },
-  //   //
-  //   // //checkhouse
-  //   // {
-  //   //   module: '细部检查',
-  //   //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
-  //   //   repositorie: 'detailcheck',
-  //   //   isOwner: false,
-  //   //   disabled: false,
-  //   // },
-  //   // {
-  //   //   module: '交付',
-  //   //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
-  //   //   repositorie: 'deilver',
-  //   //   isOwner: false,
-  //   //   disabled: false,
-  //   // },
-  //   // {
-  //   //   module: '集中交付',
-  //   //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
-  //   //   repositorie: 'focusdeilver',
-  //   //   isOwner: false,
-  //   //   disabled: false,
-  //   // },
-  //   // {
-  //   //   module: '验房参数',
-  //   //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
-  //   //   repositorie: 'checkhousepara',
-  //   //   isOwner: false,
-  //   //   disabled: false,
-  //   // },
-  //   // {
-  //   //   module: '交验房报表',
-  //   //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
-  //   //   repositorie: 'checkhousereport',
-  //   //   isOwner: false,
-  //   //   disabled: false,
-  //   // },
-  //   // //无需在菜单栏上显示
-  //   // {
-  //   //   module: '细部检查报表',
-  //   //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
-  //   //   repositorie: 'detailreport',
-  //   //   isOwner: false,
-  //   //   disabled: false,
-  //   // },
-  //   // //无需在菜单栏上显示
-  //   // {
-  //   //   module: '预交付报表',
-  //   //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
-  //   //   repositorie: 'deliverreport',
-  //   //   isOwner: false,
-  //   //   disabled: false,
-  //   // },
+  // //业户
+  // {
+  //   module: '业户',
+  //   repositorie: 'owner',
+  //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  //
+  // //checkhouse
+  // {
+  //   module: '细部检查',
+  //   repositorie: 'detailcheck',
+  //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '交付',
+  //   repositorie: 'deilver',
+  //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '集中交付',
+  //   repositorie: 'focusdeilver',
+  //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '验房参数',
+  //   repositorie: 'checkhousepara',
+  //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '交验房报表',
+  //   repositorie: 'checkhousereport',
+  //   path: 'http://192.168.1.89:3000/gaocangxiong/micro_check-house.git',
+  //   branch: 'master',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
 ];
