18a650224b04dbbdc867b48cebfd3b1efc7e37c7
xilanhuadechuntian <Lensgcx@163.com>
2019-12-13 09:32:45 +0800

    Update header-custom.vue

--- main/src/layout/header-custom.vue
+++ main/src/layout/header-custom.vue
@@ -2,20 +2,40 @@
   <div class="clear">
     <div class="fl" style="color: #fff;padding-left: 24px;font-size: 16px;">
       {{operatorName}}
+      <!--<theme-picker style="float: right;height: 26px;margin: -3px 8px 0 0;" @change="themeChange"/>-->
     </div>
   </div>
 </template>
 
 <script>
-  import {mapGetters} from 'vuex';
+  import { mapGetters } from 'vuex';
+
+  import ThemePicker from '../components/ThemePicker/index';
+  import { updateThemeColor } from '../service/System/Layout/coverPainting';
 
   export default {
     name: 'header-custom',
+    components: { ThemePicker },
     data() {
       return {};
     },
     computed: {
       ...mapGetters(['operatorName']),
+    },
+    methods: {
+      themeChange(val) {
+        console.log(22222222);
+        console.log(val);
+        console.log(22222222);
+        updateThemeColor({ themeColor: val }).then(() => {
+          let userInfo = this.$store.state.User.userinfo;
+          this.$store.dispatch('updateLoginData', userInfo);
+        });
+        // this.$store.dispatch('settings/changeSetting', {
+        //   key: 'theme',
+        //   value: val
+        // })
+      },
     },
   };
 </script>
