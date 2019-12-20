4dc52348ef3f6190abb352f4a675be2a8a37e00b
xilanhuadechuntian <Lensgcx@163.com>
2019-12-20 11:26:14 +0800

    Update index.js

--- main/src/mixins/index.js
+++ main/src/mixins/index.js
@@ -1,11 +1,11 @@
-import funcId from './System/FuncId';
-import roleButton from './System/Role-button/Role-button';
-import searchConditions from './System/Search-conditions';
+import FuncId from './System/FuncId';
+import RoleButton from './System/Role-button';
+import SearchConditions from './System/Search-conditions';
 
 // import table from './system/Table';
 
 import entrance from '../../../injection/entrance/entrance_core';
 
 export default {
-  mixins: [funcId, roleButton, searchConditions, ...entrance.mixins],
+  mixins: [FuncId, RoleButton, SearchConditions, ...entrance.mixins],
 };
