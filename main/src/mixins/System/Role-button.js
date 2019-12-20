5ea2a35f93ac19ea9484d2e186f925ea0ba97891
xilanhuadechuntian <Lensgcx@163.com>
2019-12-20 11:26:22 +0800

    Create Role-button.js

---
+++ main/src/mixins/System/Role-button.js
@@ -0,0 +1,24 @@
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['roleButtonAction', 'roleButtonForm', 'roleButtonGrid']),
+
+    gridBtns() {
+      return this.roleButtonGrid.map(({ name, code }) => ({ label: name, value: code }));
+    },
+  },
+  created() {
+    //get new role button-list
+    this.$store.dispatch('getRoleButtonList', { funcId: this.Mix_funcId }).then(data => {
+      console.log('请求到权限按钮！！！！！！！！！！！');
+      console.log(data);
+      console.log(this.tableData);
+
+      if (!this.gridBtns || !this.gridBtns.length) return;
+      if (this.tableLoader) {
+        this.tableLoader(this.gridBtns);
+      }
+    });
+  },
+};
