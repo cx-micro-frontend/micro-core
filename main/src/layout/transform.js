1dec0dff35f3d65087b9e816664ef77628a3cca8
xilanhuadechuntian <Lensgcx@163.com>
2019-12-23 14:19:31 +0800

    Update transform.js

--- main/src/layout/transform.js
+++ main/src/layout/transform.js
@@ -1,12 +1,14 @@
-import { mapGetters } from 'vuex';
 import { getUrlParam } from '../utils/library/urlhandle';
 
 export default {
-  computed: {
-    ...mapGetters(['isInIframe']),
-  },
   created() {
     const isInIframe = getUrlParam('isShowFrame') === 'false';
+
     this.$store.dispatch('setFrameMode', isInIframe);
+
+    if (isInIframe) {
+      document.getElementById('app').style.minHeight = 'auto';
+      document.getElementById('app').style.minWidth = 'auto';
+    }
   },
 };
