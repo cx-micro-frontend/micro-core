558172074ffb3ff4ea5fa7f7497c0e130d3a7fab
xilanhuadechuntian <Lensgcx@163.com>
2019-12-26 13:37:52 +0800

    [0.0.24-60] Upgrade to V-0.0.24-60

--- main/src/utils/fetch/fileFlowDistribute.js
+++ main/src/utils/fetch/fileFlowDistribute.js
@@ -4,19 +4,28 @@
  * @param callback
  */
 export const fileFlowDistribute = (response, callback) => {
-  const headers = response.headers;
   console.log(response);
-  const fileName = headers['downloadfilename'] ? decodeURI(headers['downloadfilename']) : '';
+  const headers = response.headers;
   let blob = new Blob([response.data], { type: headers['content-type'] });
-  let downloadElement = document.createElement('a');
-  let href = window.URL.createObjectURL(blob); //create a
-  downloadElement.href = href;
-  downloadElement.download = fileName ? decodeURI(fileName) : 'file.xls'; //set file name
-  document.body.appendChild(downloadElement);
-  downloadElement.click(); //click download
-  document.body.removeChild(downloadElement); //remove
-  window.URL.revokeObjectURL(href);
-  callback(response);
+  const fileName = headers['downloadfilename'] ? decodeURI(headers['downloadfilename']) : '';
+
+  //兼容ie
+  if (window.navigator.msSaveOrOpenBlob) {
+    console.log('IE 浏览器下载文件');
+    window.navigator.msSaveBlob(blob, fileName);
+    callback(response);
+  } else {
+    console.log('非 IE 浏览器下载文件');
+    let downloadElement = document.createElement('a');
+    let href = window.URL.createObjectURL(blob); //create a
+    downloadElement.href = href;
+    downloadElement.download = fileName ? decodeURI(fileName) : 'file.xls'; //set file name
+    document.body.appendChild(downloadElement);
+    downloadElement.click(); //click download
+    document.body.removeChild(downloadElement); //remove
+    window.URL.revokeObjectURL(href);
+    callback(response);
+  }
 };
 
 /**
