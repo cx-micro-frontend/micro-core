5293be512c9488a955dde41cfec6b548e81a2a27
xilanhuadechuntian <Lensgcx@163.com>
2020-02-25 12:02:14 +0800

    Update index.js

--- config/injection/index.js
+++ config/injection/index.js
@@ -18,7 +18,7 @@ const _BasicInjection = [
     module: '业务组件', //模块中文名称
     repositorie: 'biz_components', //模块唯一标识key
     path: 'http://new-see.oicp.io:14725/NEAP-components/micro_biz_components', //clone 路径
-    branch: 'arm1.0',
+    branch: 'master.ARM',
     isOwner: false, //是否为自有模块
     disabled: false, //是否禁用
   },
@@ -26,7 +26,7 @@ const _BasicInjection = [
     module: '系统设置', //模块中文名称
     repositorie: 'system', //模块唯一标识key
     path: 'http://new-see.oicp.io:14725/Framework-frontend/micro_system.git',
-    branch: 'arm1.0', //抽取分支
+    branch: 'master.ARM', //抽取分支
     isOwner: false, //是否为自有模块
     disabled: false,
   },
