513c645e4aa5058135dc5c79cd24d213e138c6a0
xilanhuadechuntian <Lensgcx@163.com>
2020-02-25 12:04:19 +0800

    Update index.js

--- config/injection/index.js
+++ config/injection/index.js
@@ -29,15 +29,7 @@ const _BasicInjection = [
     branch: 'master.ARM', //抽取分支
     isOwner: false, //是否为自有模块
     disabled: false,
-  },
-  {
-    module: '系统设置', //模块中文名称
-    repositorie: 'system', //模块唯一标识key
-    path: 'http://192.168.1.47:3000/Framework-frontend/micro_system.git', //clone 路径
-    branch: 'master',
     // landingRoot: false, //是否注入根目录中
-    isOwner: false, //是否为自有模块
-    disabled: false, //是否禁用
   },
   // {
   //   module: '测试示例', //模块中文名称
