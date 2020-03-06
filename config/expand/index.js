8beb4edef846ef8e38b6a34bcf343e5bdeaaf4a7
xilanhuadechuntian <Lensgcx@163.com>
2020-03-06 14:34:27 +0800

    expand

--- config/expand/index.js
+++ config/expand/index.js
@@ -1,5 +1,26 @@
 import headslot from './headslot';
 
 export default {
+  //头部自定义插槽
   headslot: headslot,
+
+  //自定义路由地址
+  customRoute: Layout => {
+    return [
+      {
+        path: '',
+        component: Layout,
+        redirect: '/overview',
+        name: 'overview',
+        children: [
+          {
+            path: 'overview',
+            component: () => import(`@ROOT/config/expand/overview.vue`),
+            meta: { auth: true, key: 'overview' },
+            name: '概览',
+          },
+        ],
+      },
+    ];
+  },
 };
---
+++ config/expand/overview.vue
@@ -0,0 +1,36 @@
+<!--概览页 - 外部注入个性化配置路由界面（非权限控制)-->
+<template>
+  <div class="overview">
+    <h1>概览页 - 外部注入个性化配置路由界面（非权限控制)</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'overview',
+    data() {
+      return {
+        particlesColor: '#a9abae',
+      };
+    },
+    created() {
+    },
+  };
+
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .overview {
+    position: relative;
+    height: 100%;
+    img {
+      position: absolute;
+      width: 224px;
+      height: 123px;
+      top: 50%;
+      left: 50%;
+      margin-left: -110px;
+      margin-top: -60px;
+    }
+  }
+</style>
