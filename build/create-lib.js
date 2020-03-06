c339ef3e3321bb4325decd04c6b2a51355b84bab
xilanhuadechuntian <Lensgcx@163.com>
2020-03-06 14:34:17 +0800

    Update create-lib.js

--- build/create-lib.js
+++ build/create-lib.js
@@ -12,4 +12,6 @@ if (fs.existsSync(`${utils.inJectPath().root}/main`)) {
   shell.cp('-R', `${utils.inJectPath().root}/main/src`, `${utils.inJectPath().root}/lib/src`);
   shell.cp('-R', `${utils.inJectPath().root}/main/static`, `${utils.inJectPath().root}/lib/static`);
   shell.cp('-R', `${utils.inJectPath().root}/main/main.js`, `${utils.inJectPath().root}/lib/main.js`);
+  shell.cp('-R', `${utils.inJectPath().root}/main/expand.js`, `${utils.inJectPath().root}/lib/expand.js`);
+
 }
