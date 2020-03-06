a5ccae19109961f2e001689eca9ccfff1b01209c
xilanhuadechuntian <Lensgcx@163.com>
2020-03-06 14:34:57 +0800

    Update routes.js

--- main/src/router/routes.js
+++ main/src/router/routes.js
@@ -1,5 +1,6 @@
 import Public from './modules/public';
 import User from './modules/user';
+import Custom from './modules/custom';
 // import System from './modules/system';
 
-export default [...Public, ...User];
+export default [...Public, ...User, ...Custom];
