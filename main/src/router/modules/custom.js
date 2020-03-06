702a74eef7d8bc642080d1899f43d2a8b30bff6e
xilanhuadechuntian <Lensgcx@163.com>
2020-03-06 14:35:40 +0800

    Create custom.js

---
+++ main/src/router/modules/custom.js
@@ -0,0 +1,29 @@
+import expand from '../../../expand';
+
+const Layout = resolve => require(['../../layout/Layout.vue'], resolve);
+
+export default [
+  ...expand.customRoute(Layout),
+
+  // {
+  //   path: '',
+  //   component: Layout,
+  //   redirect: '/overview',
+  //   name: 'overview',
+  //   children: [
+  //     {
+  //       path: 'overview',
+  //       component: () => import('../../views/Overview/Overview.vue'),
+  //       meta: { auth: true, key: 'overview' },
+  //       name: '主页',
+  //     },
+  //   ],
+  // },
+
+  {
+    path: '/testdemo',
+    name: 'testdemo',
+    component: () => import('../../views/TestDemo/TestDemo.vue'),
+    meta: { auth: false },
+  },
+];
