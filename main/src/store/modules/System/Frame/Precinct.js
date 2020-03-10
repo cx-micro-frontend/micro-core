b543362a840d8df12e95b172baa3fbfb9657cf21
xilanhuadechuntian <Lensgcx@163.com>
2020-03-10 15:04:02 +0800

    Update Precinct.js

--- main/src/store/modules/System/Frame/Precinct.js
+++ main/src/store/modules/System/Frame/Precinct.js
@@ -1,3 +1,14 @@
+import { storageHandle } from '../../../../utils/storage/storage';
+
+/**
+ * deCrypto side bar-information data in storage
+ * @returns {{}}
+ * @private
+ */
+function _deCryptoPrecinct() {
+  return JSON.parse(storageHandle('get', 'sign_nav')) || {};
+}
+
 /**
  * Precinct select - 头部左上角项目切换选择
  * in head slot area - select precinct item
@@ -7,20 +18,30 @@
  */
 const Precinct = {
   state: {
-    precinctId: '',
-    precinctName: '',
-    organizationId: '',
+    precinctId: _deCryptoPrecinct().precinctId || '',
+    precinctName: _deCryptoPrecinct().precinctName || '',
+    organizationId: _deCryptoPrecinct().organizationId || '',
   },
   mutations: {
     SET_PRECINCT: (state, data) => {
       state.precinctId = data.precinctId;
       state.precinctName = data.precinctName;
       state.organizationId = data.organizationId;
+      storageHandle('set', 'sign_precinct', JSON.stringify(state));
+    },
+    RESET_PRECINCT: (state, data) => {
+      state.precinctId = '';
+      state.precinctName = '';
+      state.organizationId = '';
+      storageHandle('set', 'sign_precinct', JSON.stringify(state));
     },
   },
   actions: {
     setPrecinct: ({ commit }, data) => {
       commit('SET_PRECINCT', data);
+    },
+    resetPrecinct: ({ commit }, data) => {
+      commit('RESET_PRECINCT', data);
     },
   },
 };
