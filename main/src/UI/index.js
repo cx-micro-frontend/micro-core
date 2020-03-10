12cbff460a95a0e71d0664384f0c5f42c720d38c
xilanhuadechuntian <Lensgcx@163.com>
2020-03-10 15:02:57 +0800

    UI入口增加拓展支持注入

--- main/src/UI/index.js
+++ main/src/UI/index.js
@@ -1,3 +1,4 @@
+import expand from '../../expand';
 //依赖
 import './depends';
 
@@ -15,3 +16,8 @@ import '../../../injection/entrance/entrance_ui';
 
 //load utils event
 import '../utils';
+
+//load custom iconfont
+expand.UI.iconfont();
+//load custom theme style
+expand.UI.theme();
