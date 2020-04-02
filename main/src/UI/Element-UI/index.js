614d0654364eae5445690d39a6b1bb598c3c147f
xilanhuadechuntian <Lensgcx@163.com>
2020-04-02 16:18:50 +0800

    del

--- main/src/UI/Element-UI/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-
-//base style
-import 'cx-theme/dist/cx-theme.min.css';
-//cover style
-import '../../style/cover/element-cover/index.scss';
-
-Vue.use(ElementUI);
