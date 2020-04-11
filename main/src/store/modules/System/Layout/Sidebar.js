2375d3b0c410ea7aa068d1171c5482dc053f0481
xilanhuadechuntian <Lensgcx@163.com>
2020-04-11 22:51:51 +0800

    Update Sidebar.js

--- main/src/store/modules/System/Layout/Sidebar.js
+++ main/src/store/modules/System/Layout/Sidebar.js
@@ -11,45 +11,18 @@ import expand from '../../../../../expand';
  * @private
  */
 let _baseFilterMenu = list => {
-  const labelKey = keyRefer['label'];
   const visibleKey = keyRefer['visible'];
-  const indexKey = keyRefer['menuIndex'];
   const childrenKey = keyRefer['children'];
-  const isVirtualKey = keyRefer['isVirtual'];
-  const virtualLabelKey = keyRefer['virtualLabel'];
 
   /**
    * 节点递归循环处理
    * 1、显示隐藏字段 => 转换布尔值
-   * 2、新增 - 控制是否为虚拟节点
-   * 3、若当前节点存在虚拟节点的名称的字段值，则在此节点的上面新增一个虚拟节点（用作假的二级节点，不可操作点击)
    * 显示隐藏字段 => 转换布尔值
    * @param list
    */
   const done = list => {
-    let inserts = [];
     list.forEach((item, index) => {
       item[visibleKey] = item[visibleKey] === '0';
-      item[isVirtualKey] = false;
-
-      const virtualLabe = item[virtualLabelKey];
-
-      if (item[virtualLabelKey]) {
-        inserts.push({ index: index, label: virtualLabe });
-      }
-
-      item[indexKey] = index + 1;
-
-      if (index === list.length - 1 && inserts.length) {
-        inserts.forEach((insert, i) => {
-          list.splice(insert.index + i, 0, {
-            [labelKey]: insert.label,
-            [isVirtualKey]: true,
-            [visibleKey]: false,
-            [indexKey]: 'virtual-' + (insert.index + i),
-          });
-        });
-      }
 
       let childNodes = item[childrenKey];
 
@@ -132,9 +105,10 @@ const SideBar = {
           .then(res => {
             const list = res.resultData || [];
 
-            let baselist = _baseFilterMenu(list);
+            const baselist = expand.layout.sidebar.filter(list);
 
-            let sideBarList = expand.layout.sidebar.filter(baselist);
+            const initRoute = _getInitRoute(baselist);
+            const sideBarList = _baseFilterMenu(baselist);
 
             if (!sideBarList) {
               throw '【 NEAP-ERROR 】Custom filter side menu data,  must output data list.';
@@ -142,7 +116,7 @@ const SideBar = {
 
             const navdata = {
               sideBar: sideBarList,
-              initRoute: _getInitRoute(sideBarList),
+              initRoute: initRoute,
             };
 
             commit('SET_SIDEBAR_DATA', navdata);
