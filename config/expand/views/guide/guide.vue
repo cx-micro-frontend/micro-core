989e934de1de27b2ea66934bc020949311fa6480
xilanhuadechuntian <Lensgcx@163.com>
2020-04-13 16:05:27 +0800

    Create guide.vue

---
+++ config/expand/views/guide/guide.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="win cacheDemo1">
+    <div class="ns-container">
+      导航
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'guide',
+  };
+</script>
+
+<style scoped>
+
+</style>
