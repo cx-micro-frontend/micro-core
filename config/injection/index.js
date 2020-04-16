28eba8280df41bd4144bfc6f2ad7b41570fd45ab
xilanhuadechuntian <Lensgcx@163.com>
2020-04-16 18:25:08 +0800

    Update index.js

--- config/injection/index.js
+++ config/injection/index.js
@@ -9,8 +9,11 @@
 
 const version = '0.0.1';
 
-// const prefix = 'micro_elder_';
-const prefix = 'micro_core_';
+const prefix = 'micro_elder_';
+// const prefix = 'micro_core_';
+// const prefix = 'micro_work_order_';
+
+// const prefix = 'micro_work_order_';
 
 const _VarInjection = require(`./version/${prefix}${version}`);
 
