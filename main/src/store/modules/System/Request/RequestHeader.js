3e17bcf0c4f74c34ef938ac541c6b6e8cd1e92bc
xilanhuadechuntian <Lensgcx@163.com>
2020-04-16 09:41:29 +0800

    Update RequestHeader.js

--- main/src/store/modules/System/Request/RequestHeader.js
+++ main/src/store/modules/System/Request/RequestHeader.js
@@ -1,8 +1,41 @@
+import { getToken } from '../../../../utils/library/auth';
+
 const requestHead = {
   state: {
     base: {
       appId: '07d8737811434732',
       appClientType: 'pc',
+    },
+  },
+
+  mutations: {
+    SET_SEARCG_CONDITIONS: (state, ID) => {
+      if (!ID) {
+        return false;
+      }
+      if (state.conditions.hasOwnProperty(ID)) {
+        return false;
+      }
+      state.conditions[ID] = JSON.parse(JSON.stringify(baseConditions));
+    },
+
+    EMPTY_SEARCH_CONDITIONS: (state, ID) => {
+      console.log(ID, 'empty');
+      if (state.conditions[ID]) {
+        delete state.conditions[ID];
+      }
+    },
+  },
+
+  actions: {
+    setSearchConditions({ commit, state }, ID) {
+      return new Promise(resolve => {
+        commit('SET_SEARCG_CONDITIONS', ID);
+        resolve(state.conditions[ID]);
+      });
+    },
+    emptySearchConditions({ commit }, ID) {
+      commit('EMPTY_SEARCH_CONDITIONS', ID);
     },
   },
 };
