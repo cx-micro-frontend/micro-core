be5194eef31d335d87ecd867f5f481760ba9bda1
xilanhuadechuntian <Lensgcx@163.com>
2020-06-02 16:08:14 +0800

    Update Search-conditions.js

--- main/src/mixins/System/Search-conditions.js
+++ main/src/mixins/System/Search-conditions.js
@@ -1,3 +1,5 @@
+import { baseConditions } from '../../store/modules/System/Request/SearchConditions';
+
 export default {
   data() {
     return {
@@ -12,9 +14,29 @@ export default {
 
     this.$store.dispatch('setSearchConditions', this.Mix_funcId);
     this.Mix_searchConditions = this.$store.state.Search.conditions[this.Mix_funcId];
+
+    try {
+      console.log(6666666);
+      console.log(6666666);
+
+      const UserBehaviorData = this.$store.state.UserBehavior.behavior;
+
+      console.log(UserBehaviorData);
+
+      const ps = UserBehaviorData[this.Mix_funcId].pageSize;
+
+      console.log(ps);
+      console.log(6666666);
+      console.log(6666666);
+
+      this.Mix_searchConditions.pageSize = ps || baseConditions.pageSize;
+    } catch (e) {
+      this.Mix_searchConditions.pageSize = baseConditions.pageSize;
+    }
   },
 
   beforeDestroy() {
+    // alert(1111111111);
     this.Mix_searchConditions = null;
     this.$store.dispatch('emptySearchConditions', this.Mix_funcId);
   },
