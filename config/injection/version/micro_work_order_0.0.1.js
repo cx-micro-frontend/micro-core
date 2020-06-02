5ee153fc4cf67929209da549aa49f6b34815ecea
xilanhuadechuntian <Lensgcx@163.com>
2020-06-02 16:07:33 +0800

    Update micro_work_order_0.0.1.js

--- config/injection/version/micro_work_order_0.0.1.js
+++ config/injection/version/micro_work_order_0.0.1.js
@@ -1,9 +1,22 @@
 module.exports = [
   {
+    module: '工单业务组件',
+    path: 'http://newseea.51vip.biz:3000/MDM_frontend/micro_work_order',
+    repositorie: 'NS_work_order_components',
+    isOwner: false,
+    disabled: false,
+  },
+  {
     module: '工单列表',
-    // path: 'http://username:password@new-see.oicp.io:20001/elder/micro_elder_components',//下载不了这个依赖请开启这个
     path: 'http://newseea.51vip.biz:3000/MDM_frontend/micro_work_order',
     repositorie: 'servicemanage',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '工单设置',
+    path: 'http://newseea.51vip.biz:3000/MDM_frontend/micro_work_order',
+    repositorie: 'servicesetup',
     isOwner: false,
     disabled: false,
   },
