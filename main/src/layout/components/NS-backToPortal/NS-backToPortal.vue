eaae8e333efacbd1d000b1603227a32ade16b30c
xilanhuadechuntian <Lensgcx@163.com>
2020-06-10 21:26:28 +0800

    Create NS-backToPortal.vue

---
+++ main/src/layout/components/NS-backToPortal/NS-backToPortal.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="ns-back-to-portal">
+     <ns-icon-svg icon-class="suo"
+                  @click="back"
+     ></ns-icon-svg>
+  </span>
+
+</template>
+
+<script>
+  export default {
+    name: 'ns-back-to-portal',
+    methods: {
+      back() {
+        this.$router.push({ path: '/portal' });
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .ns-back-to-portal {
+
+  }
+</style>
