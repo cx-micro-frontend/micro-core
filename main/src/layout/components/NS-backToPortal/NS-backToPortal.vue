a2085c81f6c9404ee6da14760328838da9111e8a
xilanhuadechuntian <Lensgcx@163.com>
2020-06-10 22:10:01 +0800

    Update NS-backToPortal.vue

--- main/src/layout/components/NS-backToPortal/NS-backToPortal.vue
+++ main/src/layout/components/NS-backToPortal/NS-backToPortal.vue
@@ -1,25 +1,61 @@
 <template>
-  <span class="ns-back-to-portal">
-     <ns-icon-svg icon-class="suo"
-                  @click="back"
-     ></ns-icon-svg>
-  </span>
+  <div :class="[
+  'ns-back-to-portal',
+  {'is-active':isActive}
+  ]">
+    <ns-icon-class class="back-to-portal-icon"
+                   icon-class="el-icon-s-home"
+                   @click="back"
+    ></ns-icon-class>
+
+    <span>首页</span>
+  </div>
 
 </template>
 
 <script>
   export default {
     name: 'ns-back-to-portal',
+    computed: {
+      isActive() {
+        return this.$route.name === 'portal';
+      },
+    },
     methods: {
       back() {
         this.$router.push({ path: '/portal' });
       },
     },
+    created() {
+
+    },
   };
 </script>
 
-<style scoped>
+<style rel="stylesheet/scss" lang="scss">
+  $back-color_active:#f2f2f2;
   .ns-back-to-portal {
+    cursor: pointer;
+    padding: 0 5px;
+    &:hover {
+      background: $back-color_active;
+    }
+    &.is-active {
+      background: $back-color_active;
+    }
+    //铃铛
+    i.ns-icon-class {
+      font-size: 22px;
+      width: 20px;
+      height: 20px;
+      vertical-align: -0.1em;
+      padding: 0;
+      color: #222222;
+    }
+    span {
+      font-weight: bold;
+      color: #222222;
+    }
 
   }
 </style>
