6810c0e00df9bb3be03ffec16fbfdd1e25d11945
xilanhuadechuntian <Lensgcx@163.com>
2020-06-16 11:34:56 +0800

    Create _import_beifen.js

---
+++ main/src/router/_import/_import_beifen.js
@@ -0,0 +1,23 @@
+const modules = require('@ROOT/config/injection/index').modules;
+
+module.exports = (childPath, fatherPath) => {
+  const currentModule = modules.filter(m => m.repositorie === fatherPath);
+
+  const isOwner = currentModule[0].isOwner;
+  const isLandingRoot = currentModule[0].landingRoot;
+
+  const moduleName = fatherPath ? `NS_${fatherPath}/` : '';
+
+  if (isOwner || isLandingRoot) {
+    // console.log('引入 自有  模块路由');
+    // console.log(`@ROOT/${moduleName}views/${fatherPath}/${childPath}/${childPath}.vue`)
+    return () => import(`@ROOT/${moduleName}views/${fatherPath}/${childPath}/${childPath}.vue`);
+  } else {
+    // console.log('引入 外部  模块路由')
+    // console.log(`../../../injection/repositories/${moduleName}views/${fatherPath}/${childPath}/${childPath}.vue`)
+    return () =>
+      import(
+        `../../../../injection/repositories/${moduleName}views/${fatherPath}/${childPath}/${childPath}.vue`
+      );
+  }
+};
