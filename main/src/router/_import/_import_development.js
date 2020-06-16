5f9b402809ff625a11d2ab5a8b0e6b5d1dc054b9
xilanhuadechuntian <Lensgcx@163.com>
2020-06-16 11:34:59 +0800

    Update _import_development.js

--- main/src/router/_import/_import_development.js
+++ main/src/router/_import/_import_development.js
@@ -9,29 +9,29 @@ module.exports = (firstPath, secondPath) => {
   const moduleName = firstPath ? `NS_${firstPath}/` : '';
 
   if (isOwner || isLandingRoot) {
-    console.log('引入 自有  模块路由');
+    // console.log('引入 自有  模块路由');
 
     if (secondPath) {
-      console.log(`@ROOT/${moduleName}views/${firstPath}/${secondPath}/${secondPath}.vue`);
+      // console.log(`@ROOT/${moduleName}views/${firstPath}/${secondPath}/${secondPath}.vue`);
       return () => import(`@ROOT/${moduleName}views/${firstPath}/${secondPath}/${secondPath}.vue`);
     } else {
-      console.log(`@ROOT/${moduleName}views/${firstPath}/${firstPath}.vue`);
+      // console.log(`@ROOT/${moduleName}views/${firstPath}/${firstPath}.vue`);
       return () => import(`@ROOT/${moduleName}views/${firstPath}/${firstPath}.vue`);
     }
   } else {
-    console.log('引入 外部  模块路由');
+    // console.log('引入 外部  模块路由');
     if (secondPath) {
-      console.log(
-        `../../../injection/repositories/${moduleName}views/${firstPath}/${secondPath}/${secondPath}.vue`
-      );
+      // console.log(
+      //   `../../../injection/repositories/${moduleName}views/${firstPath}/${secondPath}/${secondPath}.vue`
+      // );
       return () =>
         import(
           `../../../../injection/repositories/${moduleName}views/${firstPath}/${secondPath}/${secondPath}.vue`
         );
     } else {
-      console.log(
-        `../../../injection/repositories/${moduleName}views/${firstPath}/${firstPath}.vue`
-      );
+      // console.log(
+      //   `../../../injection/repositories/${moduleName}views/${firstPath}/${firstPath}.vue`
+      // );
       return () =>
         import(
           `../../../../injection/repositories/${moduleName}views/${firstPath}/${firstPath}.vue`
