9222025938e498143a8527c5ca00a63fadaa1a06
xilanhuadechuntian <Lensgcx@163.com>
2020-07-01 15:40:08 +0800

    libiary 新增 debounce 节流

--- main/src/utils/library/event.js
+++ main/src/utils/library/event.js
@@ -43,3 +43,38 @@ export const stopPropagation = e => {
     window.event.cancelBubble = true; //IE stop propagation
   }
 };
+
+/**
+ * debounce
+ * @param func
+ * @param wait
+ * @param immediate
+ * @returns {Function}
+ */
+export const debounce = (func, wait, immediate) => {
+  let timeout, args, context, timestamp, result;
+
+  const later = function() {
+    const last = +new Date() - timestamp;
+    if (last < wait && last > 0) {
+      timeout = setTimeout(later, wait - last);
+    } else {
+      timeout = null;
+      if (!immediate) {
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      }
+    }
+  };
+  return function(...args) {
+    context = this;
+    timestamp = +new Date();
+    const callNow = immediate && !timeout;
+    if (!timeout) timeout = setTimeout(later, wait);
+    if (callNow) {
+      result = func.apply(context, args);
+      context = args = null;
+    }
+    return result;
+  };
+};
