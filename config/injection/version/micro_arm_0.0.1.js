6c8e5db1e937931907bd905f59ea1a7a14f7eaa5
xilanhuadechuntian <Lensgcx@163.com>
2020-07-16 16:47:36 +0800

    Create micro_arm_0.0.1.js

---
+++ config/injection/version/micro_arm_0.0.1.js
@@ -0,0 +1,133 @@
+module.exports = [
+  //门户
+  {
+    module: '门户页',
+    repositorie: 'portal',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro_portal.git',
+    isOwner: false,
+    disabled: false,
+  },
+
+  //业户
+  {
+    module: '业户',
+    repositorie: 'owner',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+
+  // V10 - 应收中台 - 财务收费
+  {
+    module: '应收中台组件库',
+    repositorie: 'arm_components',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '消息管理',
+    repositorie: 'messageManagement',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '项目管理',
+    repositorie: 'service',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '票据',
+    repositorie: 'bill',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '报表',
+    repositorie: 'reporting',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '退款',
+    repositorie: 'refund',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '凭证',
+    repositorie: 'voucherBatch',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '权责应收',
+    repositorie: 'power',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '仪表',
+    repositorie: 'Meter',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '计费',
+    repositorie: 'charge',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '收款',
+    repositorie: 'pay',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro-arm.git',
+    branch: 'V1.0.20200611.ARM.RC',
+    isOwner: false,
+    disabled: false,
+  },
+
+  // 工单模块
+  {
+    module: '工单业务组件库',
+    repositorie: 'work_order_components',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '工单列表',
+    repositorie: 'servicemanage',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
+    isOwner: false,
+    disabled: false,
+  },
+  {
+    module: '工单设置',
+    repositorie: 'servicesetup',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
+    isOwner: false,
+    disabled: false,
+  },
+];
