32a70fd40f17e28c89c0b848e3aead54f2caefb0
xilanhuadechuntian <Lensgcx@163.com>
2020-07-22 13:49:06 +0800

    Update micro_all_0.0.1.js

--- config/injection/version/micro_all_0.0.1.js
+++ config/injection/version/micro_all_0.0.1.js
@@ -109,25 +109,25 @@ module.exports = [
   },
 
   // 工单模块
-  {
-    module: '工单业务组件库',
-    repositorie: 'work_order_components',
-    path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
-    isOwner: false,
-    disabled: false,
-  },
-  {
-    module: '工单列表',
-    repositorie: 'servicemanage',
-    path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
-    isOwner: false,
-    disabled: false,
-  },
-  {
-    module: '工单设置',
-    repositorie: 'servicesetup',
-    path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
-    isOwner: false,
-    disabled: false,
-  },
+  // {
+  //   module: '工单业务组件库',
+  //   repositorie: 'work_order_components',
+  //   path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '工单列表',
+  //   repositorie: 'servicemanage',
+  //   path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
+  // {
+  //   module: '工单设置',
+  //   repositorie: 'servicesetup',
+  //   path: 'http://47.96.187.51:3001/NW-frontend/micro_work_order.git',
+  //   isOwner: false,
+  //   disabled: false,
+  // },
 ];
