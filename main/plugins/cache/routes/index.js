0a623ec90a88f7e5d30c469123b6556478ef68d8
xilanhuadechuntian <Lensgcx@163.com>
2020-08-12 09:37:32 +0800

    Update index.js

--- main/plugins/cache/routes/index.js
+++ main/plugins/cache/routes/index.js
@@ -11,8 +11,27 @@ export default (
       component: options.Layout,
       children: [
         {
+          name: 'NEAP_redirect',
           path: '/NEAP_redirect/:path*',
           component: () => import('./redirect.vue'),
+          meta: { key: 'NEAP_redirect' },
+          beforeEnter: (to, from, next) => {
+            // console.log(33333333333);
+            // console.log(to);
+            // console.log(from);
+            // const { params, query } = to;
+            // const { path } = params;
+            //
+            //
+            // // this.$router.replace({ path: '/' + path, query });
+            //
+            // console.log(66666666666);
+            // console.log(path);
+            // console.log(query);
+            // console.log(66666666666);
+            next();
+            // next({ path: '/' + path, query});
+          },
         },
       ],
     },
