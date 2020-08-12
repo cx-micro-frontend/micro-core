f8cc70e1b5627b648d875fe0e94aebefa226e334
xilanhuadechuntian <Lensgcx@163.com>
2020-08-12 14:04:23 +0800

    Update index.js

--- main/src/components/index.js
+++ main/src/components/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue';
 
-//select-tree
-import BizOrgnizeSelectTree from './NS-biz-select-tree/Biz-organize-select-tree/Biz-organize-select-tree.vue';
-
 import BizRoleButtonArea from './NS-biz-role-button-area/Biz-role-button-area.vue';
-
-//Navigation
-// import dropdown from './NS-biz-Navigation/NS-biz-Navigation.vue';
-
-//select tree
-Vue.component('ns-biz-organize-select-tree', BizOrgnizeSelectTree);
 
 Vue.component('ns-biz-role-button-area', BizRoleButtonArea);
 
 //Navigation
 // Vue.component('ns-dropdown', dropdown);
+
+//Navigation
+// import dropdown from './NS-biz-Navigation/NS-biz-Navigation.vue';
