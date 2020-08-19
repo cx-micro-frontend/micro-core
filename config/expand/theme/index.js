5885fd38502ffcf0d4aef62d9e890b14292f7a9d
xilanhuadechuntian <Lensgcx@163.com>
2020-08-19 08:43:31 +0800

    主题更新

--- config/expand/theme/index.js
+++ config/expand/theme/index.js
@@ -1,6 +1,11 @@
 //base style
 import './assets/iconfont/element-icons.ttf';
 import './assets/iconfont/element-icons.woff';
-import './elder-theme.min.css';
+
+import './arm-theme.min.css';
 //cover style
-import './elder-cover/index.scss';
+import './arm-cover/index.scss';
+
+// import './elder-theme.min.css';
+// //cover style
+// import './elder-cover/index.scss';
