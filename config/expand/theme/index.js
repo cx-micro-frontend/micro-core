4430b12b7baff0a2f919d993da338ff4332830da
xilanhuadechuntian <Lensgcx@163.com>
2020-08-21 14:48:35 +0800

    样式更新

--- config/expand/theme/index.js
+++ config/expand/theme/index.js
@@ -5,7 +5,3 @@ import './assets/iconfont/element-icons.woff';
 import './arm-theme.min.css';
 //cover style
 import './arm-cover/index.scss';
-
-// import './elder-theme.min.css';
-// //cover style
-// import './elder-cover/index.scss';
