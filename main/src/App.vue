b77ce408f650b5c7cd20a08d7c4fa260dd5d4cdb
xilanhuadechuntian <Lensgcx@163.com>
2020-09-16 15:07:53 +0800

    App 加tag 标识

--- main/src/App.vue
+++ main/src/App.vue
@@ -6,22 +6,38 @@
 */
 
 <template>
-  <div id="app">
+  <div id="app" :class="appName" :tag="appTag">
     <router-view/>
   </div>
 </template>
 
 <script>
+  const packageJson = require('../../package.json');
+
   export default {
     name: 'app',
     created() {
       this.$store.dispatch('setBrowserInfo'); //设置浏览器信息
       this.$store.dispatch('setOSInfo'); //设置操作系统信息
     },
+    computed: {
+      projectName() {
+        return packageJson.name;
+      },
+      routeName() {
+        return `NEAP-route__${this.$route.name}`;
+      },
+      appName() {
+        return `${this.projectName} ${this.routeName}`;
+      },
+      appTag() {
+        return `${this.projectName}-${packageJson.tag || 'normal'}`;
+      },
+    },
   };
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
+<style rel="stylesheet/scss" lang="scss" scoped>
   #app {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
