218e19fd121edeb97f58c3f951c96b770894171f
xilanhuadechuntian <Lensgcx@163.com>
2020-09-28 18:05:37 +0800

    Update index.js

--- main/src/layout/index.js
+++ main/src/layout/index.js
@@ -1,12 +1,7 @@
-export { default as nsTopMenu } from './components/NS-nav-menu/NS-top-menu/NS-top-menu';
-
-// export { default as bizSidebar } from './components/NS-nav-menu/NS-side-menu/biz-sidebar';
+export { default as nsHeader } from './components/NS-header/index.vue';
 export { default as bizSideMenu } from './components/NS-nav-menu/NS-side-menu/index';
 
 export { default as bizTabsViews } from './components/NS-biz-page-tabs/index';
-export { default as bizLockScreen } from './components/NS-biz-lockscreen/NS-biz-lockscreen';
-export { default as bizSkiner } from './components/NS-theme/index';
-export { default as bizUserDropdown } from './components/NS-biz-user-dropdown/NS-biz-user-dropdown';
 
 export { default as headerSlotLeft } from './components/NS-header-slot/NS-header-slot-left';
 export { default as headerSlotRight } from './components/NS-header-slot/NS-header-slot-right';
