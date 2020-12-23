62fb48114d23aa53445984efd9a1f80706cdbc88
xilanhuadechuntian <Lensgcx@163.com>
2020-12-23 11:29:07 +0800

    Update topNavMenuData.js

--- main/src/layout/mixins/topNavMenuData.js
+++ main/src/layout/mixins/topNavMenuData.js
@@ -1,6 +1,9 @@
 import { mapGetters } from 'vuex';
 import { createTopMenu } from '../../layout/components/NS-nav-menu/utils/dataHandle';
 
+/**
+ * 顶部模块子系统菜单数据的生成和获取
+ */
 export default {
   computed: {
     ...mapGetters(['moduleMenu']),
