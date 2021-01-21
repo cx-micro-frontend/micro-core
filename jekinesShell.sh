5d58ddbb4abd851fe9cc3219484453ab60595e20
xilanhuadechuntian <Lensgcx@163.com>
2021-01-21 17:50:10 +0800

    Update jekinesShell.sh

--- jekinesShell.sh
+++ jekinesShell.sh
@@ -1,22 +1,44 @@
 #Design by Broccoli spring( gcx-高仓雄 ) <Lensgcx@163.com>
 #!/usr/bin/env bash
 
-#子系统（一级大模块系统，如：arm，system，check_house，work_order等)
+#=====================================================================
+#子系统过滤抽取配置
+#即：一级大模块系统，如：arm，system，check_house，work_order等
+#注意：如果不需要配置，请将下列代码注释掉即可，如果想打全量包，则无需配置任何参数
+#=====================================================================
 SUBSYSTEMS_LIST='"arm","system","check_house","work_order"'
+#SUBSYSTEMS_LIST='"system"'
 
-SUBSYSTEMS_LIST='"system"'
 
-
-#子模块（内部子模块，如：owner，system，charge，report 等)
+#=====================================================================
+#子模块过滤抽取配置
+#即：内部子模块，如：owner，system，charge，report 等
+#注意：如果不需要配置，请将下列代码注释掉即可，如果想打全量包，则无需配置任何参数
+#=====================================================================
 MODULES_LIST='"quality_components","projectCheck","system"'
 
-#头部工具条（如：)
+
+#=====================================================================
+#头部工具条过滤抽取配置
+#即：如：消息，换肤，工具条等
+#注意：如果不需要配置，请将下列代码注释掉即可，如果想打全量包，则无需配置任何参数
+#=====================================================================
 HEADTOOLBAR_LIST='"owner","charge","report","system1231"'
 
 
-S='{"SUBSYSTEMS_LIST":['${SUBSYSTEMS_LIST}']}'
-M='{"MODULES_LIST":['${MODULES_LIST}']}'
-H='{"HEADTOOLBAR_LIST":['${HEADTOOLBAR_LIST}']}'
+function transform {
+    if [ $1 ];
+    then
+       echo "[$1]"
+    else
+       echo "null"
+    fi
+}
+
+S='{"SUBSYSTEMS_LIST":'$(transform ${SUBSYSTEMS_LIST})'}'
+M='{"MODULES_LIST":'$(transform ${MODULES_LIST})'}'
+H='{"HEADTOOLBAR_LIST":'$(transform ${HEADTOOLBAR_LIST})'}'
+
 
 #cross-env MODULES_LIST=${M} SUBSYSTEMS_LIST=${S} HEADTOOLBAR_LIST=${H} node ./injecter/injectionList/index.js
 
