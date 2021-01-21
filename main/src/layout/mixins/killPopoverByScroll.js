5d663ff0e322df3723822abf53c498b3018abd58
xilanhuadechuntian <Lensgcx@163.com>
2021-01-21 15:41:23 +0800

    Update killPopoverByScroll.js

--- main/src/layout/mixins/killPopoverByScroll.js
+++ main/src/layout/mixins/killPopoverByScroll.js
@@ -43,13 +43,48 @@ const mousedownHandle = e => {
 };
 
 /**
+ * 获取事件冒泡路径，兼容ie11,edge,chrome,firefox,safari
+ * @param evt
+ * @returns {*}
+ */
+function eventPath(evt) {
+  const path = (evt.composedPath && evt.composedPath()) || evt.path,
+    target = evt.target;
+
+  if (path != null) {
+    return path.indexOf(window) < 0 ? path.concat(window) : path;
+  }
+
+  if (target === window) {
+    return [window];
+  }
+
+  function getParents(node, memo) {
+    memo = memo || [];
+    const parentNode = node.parentNode;
+
+    if (!parentNode) {
+      return memo;
+    } else {
+      return getParents(parentNode, memo.concat(parentNode));
+    }
+  }
+
+  return [target].concat(getParents(target), window);
+}
+
+/**
  * mousewheelHandle
  * @param e
  */
 const mousewheelHandle = e => {
+  console.log(e);
+  console.log(lock);
+
   if (lock) return;
 
-  let checkPopper = (e.path || []).some(dom => {
+  const path = eventPath(e);
+  let checkPopper = (path || []).some(dom => {
     return (dom.classList || []).contains('el-popper');
   });
 
