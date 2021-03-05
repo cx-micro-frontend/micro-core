7921369d456419a18576825d7952e11e417c4dbd
xilanhuadechuntian <Lensgcx@163.com>
2021-03-05 09:27:25 +0800

    Update create-lib.js

--- build/src/create-lib.js
+++ build/src/create-lib.js
@@ -8,25 +8,21 @@ const shell = require('shelljs');
 const mainPath = path.resolve('main');
 const libPath = path.resolve('lib');
 
+//删除 lib 文件夹
+shell.rm('-rf', libPath);
+//创建 lib 文件夹
+fs.mkdirSync(libPath);
 
-/**
- * 有lib删除，无lib则创建
- */
-if (fs.existsSync(libPath)) {
-  shell.rm('-rf', path);//清除lib
-}
-else {
-  fs.mkdirSync(libPath);
+if (fs.existsSync(mainPath)) {
+  shell.cp('-R', `${mainPath}/*`, `${libPath}`);
 }
 
-if (fs.existsSync(`${path.resolve('')}/main`)) {
 
-  shell.cp('-R', `${mainPath}/mock`, `${libPath}/mock`);
-  shell.cp('-R', `${mainPath}/src`, `${libPath}/src`);
-  shell.cp('-R', `${mainPath}/static`, `${libPath}/static`);
-  shell.cp('-R', `${mainPath}/main.js`, `${libPath}/main.js`);
-  shell.cp('-R', `${mainPath}/expand.js`, `${libPath}/expand.js`);
-  shell.cp('-R', `${mainPath}/dependencies.js`, `${libPath}/dependencies.js`);
-  shell.cp('-R', `${mainPath}/plugins`, `${libPath}/plugins`);
 
-}
+// shell.cp('-R', `${mainPath}/mock`, `${libPath}/mock`);
+// shell.cp('-R', `${mainPath}/src`, `${libPath}/src`);
+// shell.cp('-R', `${mainPath}/static`, `${libPath}/static`);
+// shell.cp('-R', `${mainPath}/main.js`, `${libPath}/main.js`);
+// shell.cp('-R', `${mainPath}/expand.js`, `${libPath}/expand.js`);
+// shell.cp('-R', `${mainPath}/dependencies.js`, `${libPath}/dependencies.js`);
+// shell.cp('-R', `${mainPath}/plugins`, `${libPath}/plugins`);
