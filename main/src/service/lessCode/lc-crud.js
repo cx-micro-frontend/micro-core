1d91ac1340f8db48c4a7e5fca50eac6d7685fb78
BroccoliSpring <Lensgcx@163.com>
2021-03-23 17:17:46 +0800

    Create lc-crud.js

---
+++ main/src/service/lessCode/lc-crud.js
@@ -0,0 +1,94 @@
+import $store from '../../store';
+import fetch from '../../utils/fetch/fetch';
+import { dataFilter } from '../../utils/fetch/fetchDataType';
+
+const { formName } = $store.getters.currentPageInfo || {};
+
+/**
+ * 获取 表单json渲染模板 和 其他信息（dialog 尺寸） 的接口
+ * @param context
+ */
+export function getForm(context) {
+  // const { appCode } = context; //appCode:应用编码;
+
+  const appCode = 'nlcpd'; //后续要改成  system
+
+  return fetch({
+    url: `/${appCode}/base/getFormInfo`,
+    method: 'get',
+  });
+}
+
+/**
+ * 新增
+ * @param context
+ * @param query
+ */
+export function addItem(context, query) {
+  dataFilter(query);
+  const { appCode } = context; //appCode:应用编码;
+  return fetch({
+    url: `/${appCode}/${formName}/add-${formName}`,
+    method: 'post',
+    data: query,
+  });
+}
+
+/**
+ * 编辑
+ * @param context
+ * @param query
+ */
+export function editItem(context, query) {
+  dataFilter(query);
+  const { appCode } = context; //appCode:应用编码;
+  return fetch({
+    url: `/${appCode}/${formName}/edit-${formName}`,
+    method: 'post',
+    data: query,
+  });
+}
+
+/**
+ * 删除
+ * @param context
+ * @param id
+ */
+export function deleteItem(context, id) {
+  dataFilter(id);
+  const { appCode } = context; //appCode:应用编码;
+  return fetch({
+    url: `/${appCode}/${formName}/delete-${formName}?id=${id}`,
+    method: 'delete',
+  });
+}
+
+/**
+ * 批量删除
+ * @param context
+ * @param query
+ */
+export function batchDelete(context, query) {
+  dataFilter(query);
+  const { appCode } = context; //appCode:应用编码;
+  return fetch({
+    url: `/${appCode}/${formName}/delete-${formName}-batch`,
+    method: 'delete',
+    data: query,
+  });
+}
+
+/**
+ * 获取详情
+ * @param context
+ * @param params
+ */
+export function detailItem(context, params) {
+  dataFilter(params);
+  const { appCode } = context; //appCode:应用编码;
+  return fetch({
+    url: `/${appCode}/${formName}/detail-${formName}`,
+    method: 'get',
+    params: params,
+  });
+}
