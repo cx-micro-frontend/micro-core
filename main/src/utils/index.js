963e34c68af1ba27d92a4552296e13269f4d90da
BroccoliSpring <Lensgcx@163.com>
2021-03-23 17:18:06 +0800

    Update index.js

--- main/src/utils/index.js
+++ main/src/utils/index.js
@@ -1,39 +1,5 @@
+import fnModuleExport from './core/fnModuleExport';
+
 const libraryFiles = require.context('./library', true, /\.js/);
 
-let modules = {};
-
-libraryFiles.keys().forEach(key => {
-  //file modules object
-  const m = libraryFiles(key);
-
-  /**
-   * export not has default => {}
-   * default => method
-   * @type {string}
-   */
-  const methodItem = Object.keys(m).reduce((s, e) => {
-    if (e !== 'default') {
-      if (typeof m[e] === 'function') {
-        modules[e] = m[e];
-        return s;
-      } else s[e] = m[e];
-    }
-    return s;
-  }, m.default || {});
-
-  if (methodItem && typeof methodItem === 'function') {
-    const fileName = key.replace(/(.*\/)*([^.]+).*/gi, '$2');
-
-    if (modules.hasOwnProperty('fileName')) {
-      throw 'entry script modules has the same method';
-    }
-
-    modules[fileName] = methodItem;
-  }
-});
-
-// console.log('控制反转脚本模块');
-// console.log(modules);
-// console.log('控制反转脚本模块');
-
-export default modules;
+export default fnModuleExport(libraryFiles);
