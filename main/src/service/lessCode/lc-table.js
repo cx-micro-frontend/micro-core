b3dff23f1ed613123423ca79d4d4810af9322dad
BroccoliSpring <Lensgcx@163.com>
2021-03-31 16:19:29 +0800

    [0.0.1-46] Upgrade to V-0.0.1-46

--- main/src/service/lessCode/lc-table.js
+++ main/src/service/lessCode/lc-table.js
@@ -1,6 +1,5 @@
 import { dataFilter } from '../../utils/fetch/fetchDataType';
-import fetch from '../../utils/fetch/fetch';
-import { basicFetchHeader, codeParame } from './index';
+import { lessCodeBasicFetch, codeParame } from './index';
 
 /**
  * table data fetch
@@ -16,12 +15,10 @@ export const tableDataFetch = (context, data) => {
    */
   const { appCode, formName } = codeParame(context);
 
-  return fetch({
-    ...basicFetchHeader,
+  return lessCodeBasicFetch(codeParame(context), {
     url: data.url || `/${appCode}/${formName}/list-${formName}`,
     method: data.method || 'post',
     headers: {
-      funcId: data.funcId,
       interceptorType: true, //当前页面是第二次进来的情况 && searchConditions 对象没有发生变化（用户没有操作分页或者更改任何查询条件）
     },
     data: data.query,
