2505894d23ebeba2fc31dc0e2606a6eb1a970877
BroccoliSpring <Lensgcx@163.com>
2021-05-11 16:31:36 +0800

    引入uuid库，增加uuid方法

--- main/src/utils/library/uuid.js
+++ main/src/utils/library/uuid.js
@@ -1,7 +1,13 @@
-export const uuid = length => {
-  return Number(
-    Math.random()
-      .toString()
-      .substr(3, length) + Date.now()
-  ).toString(36);
+import { v4 as uuidv4 } from 'uuid';
+
+export const uuid = () => {
+  return uuidv4();
 };
+
+// export const uuid = length => {
+//   return Number(
+//     Math.random()
+//       .toString()
+//       .substr(3, length) + Date.now()
+//   ).toString(36);
+// };
