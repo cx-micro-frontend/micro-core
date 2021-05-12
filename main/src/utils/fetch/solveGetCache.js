45031d58162087e28e667a16c21e0a6cdbcae45d
BroccoliSpring <Lensgcx@163.com>
2021-05-13 00:15:52 +0800

    Update solveGetCache.js

--- main/src/utils/fetch/solveGetCache.js
+++ main/src/utils/fetch/solveGetCache.js
@@ -4,7 +4,8 @@ import { uuid } from '../library/uuid';
 export default config => {
   if (config.method === 'get' && getBrowserInfo().browser === 'ie') {
     config.params = Object.assign(config.params || {}, {
-      _: new Date().getTime(),
+      _: uuid(),
+      // _: new Date().getTime(),
     });
   }
 };
