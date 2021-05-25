085c2711bea18ffb5056afd75bead018d20814c8
BroccoliSpring <Lensgcx@163.com>
2021-05-25 11:03:54 +0800

    Create lc-group.js

---
+++ main/src/service/lessCode/lc-group.js
@@ -0,0 +1,17 @@
+import { dataFilter } from '../../utils/fetch/fetchDataType';
+import { lessCodeBasicFetch, codeParame } from './index';
+
+/**
+ * 筛选器 分组控件的获取数量
+ * @param context
+ * @param data
+ */
+export const getGroupCount = (context, data) => {
+  dataFilter(data);
+  const { appCode, formName } = codeParame(context);
+  return lessCodeBasicFetch(codeParame(context), {
+    url: `/${appCode}/${formName}/list-groupByCount`,
+    method: 'post',
+    data,
+  });
+};
