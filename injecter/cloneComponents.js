73698c77241c2bf0d83bfbe8c7a8e62674ae5f9e
BroccoliSpring <Lensgcx@163.com>
2021-06-02 10:27:28 +0800

    Delete cloneComponents.js

--- injecter/cloneComponents.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var path = require('path');
-var injectConfigList = require('neap-core/lib/injecter/injectionList');
-var clone = require('neap-core/lib/injecter/clone');
-
-clone.cloneRepositories(injectList);
