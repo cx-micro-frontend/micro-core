9a698d9959e9f54c73eede684fc0b46f25a16008
BroccoliSpring <Lensgcx@163.com>
2021-06-02 10:23:09 +0800

    Create cloneComponents.js

---
+++ injecter/cloneComponents.js
@@ -0,0 +1,6 @@
+'use strict';
+var path = require('path');
+var injectConfigList = require('neap-core/lib/injecter/injectionList');
+var clone = require('neap-core/lib/injecter/clone');
+
+clone.cloneRepositories(injectList);
