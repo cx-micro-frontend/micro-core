704c47a295cc05dcb82a977a4f57438d3f5c0855
BroccoliSpring <Lensgcx@163.com>
2021-06-02 10:56:38 +0800

    [0.0.1-65] Upgrade to V-0.0.1-65

--- config/injection/index.js
+++ config/injection/index.js
@@ -19,8 +19,8 @@ const _BasicInjection = [
     module: '业务组件', //模块中文名称
     repositorie: 'biz_components', //模块唯一标识key
     rootRepositorie: 'biz_components', //根模块唯一标识key
-    path: 'http://new-see.oicp.io:14725/NEAP-components/micro_biz_components', //clone 路径
-    branch: 'master.ARM',
+    path: 'http://47.96.187.51:3001/NW-frontend/micro_biz_components', //clone 路径
+    branch: 'v1.0.NEAP.BASIC.DEV',
     isOwner: false, //是否为自有模块
     disabled: false, //是否禁用
   },
@@ -29,8 +29,8 @@ const _BasicInjection = [
     module: '系统设置', //模块中文名称
     repositorie: 'system', //模块唯一标识key
     rootRepositorie: 'system', //根模块唯一标识key
-    path: 'http://new-see.oicp.io:14725/Framework-frontend/micro_system.git',
-    branch: 'master.ARM', //抽取分支
+    path: 'http://47.96.187.51:3001/NW-frontend/micro_system.git',
+    branch: 'v1.0.NEAP.SYSTEM.RC', //抽取分支
     isOwner: false, //是否为自有模块
     disabled: false,
   },
