10bc60e65954d5f01c7f5136f24797d5e1c31063
BroccoliSpring <Lensgcx@163.com>
2021-07-13 16:53:22 +0800

    del

--- main/dependencies.tsx
+++
@@ -1,3 +0,0 @@
-export const modules = require('@ROOT/config/injection/index').modules;
-
-export const injection = require('@ROOT/config/injection/index.js');
--- main/src/config.js
+++
@@ -1,8 +0,0 @@
-/*
- * 系统全局性配置
- * @type {string}
- * ------------------------
- */
-
-// export const iniView = '/front/login'; //初始路由地址
-// export const expandFilePath = '@ROOT/config/expand'; //扩展支持文件路径
