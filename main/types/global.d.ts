2fcee048679aa829f5579f1cf3a1a002c53fdf4e
BroccoliSpring <Lensgcx@163.com>
2021-07-16 15:21:07 +0800

    增加类型诊断

--- main/types/global.d.ts
+++ main/types/global.d.ts
@@ -1,31 +1,31 @@
-declare module '*.svg' {
-  interface Svg {
-    content: string;
-    id: string;
-    viewBox: string;
-    node: any;
-  }
-
-  const svg: Svg;
-  export default svg;
-}
-
-declare module '*.png' {
-  const png: string;
-  export default png;
-}
-
-declare module '*.mp3' {
-  const mp3: string;
-  export default mp3;
-}
-
-declare module '*.mp4' {
-  const mp3: string;
-  export default mp4;
-}
-
-declare module '*.gif' {
-  const png: string;
-  export default png;
-}
+// declare module '*.svg' {
+//   interface Svg {
+//     content: string;
+//     id: string;
+//     viewBox: string;
+//     node: any;
+//   }
+//
+//   const svg: Svg;
+//   export default svg;
+// }
+//
+// declare module '*.png' {
+//   const png: string;
+//   export default png;
+// }
+//
+// declare module '*.mp3' {
+//   const mp3: string;
+//   export default mp3;
+// }
+//
+// declare module '*.mp4' {
+//   const mp3: string;
+//   export default mp4;
+// }
+//
+// declare module '*.gif' {
+//   const png: string;
+//   export default png;
+// }
