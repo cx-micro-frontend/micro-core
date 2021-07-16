e624f957b5d3445cfd092b119df9f8d8c79a89ab
BroccoliSpring <Lensgcx@163.com>
2021-07-16 15:21:17 +0800

    Update head-slot-mixins.js

--- main/src/layout/components/NS-header-slot/head-slot-mixins.js
+++ main/src/layout/components/NS-header-slot/head-slot-mixins.js
@@ -4,7 +4,7 @@ import expand from '../../../../expand';
 export default {
   computed: {
     // hasFile() {
-    //   return hasFile('@ROOT/config/expand', /\config.js/);
+    //   return hasFile('@ROOT/config/expand', /\config.ts/);
     // },
 
     slotKey() {
