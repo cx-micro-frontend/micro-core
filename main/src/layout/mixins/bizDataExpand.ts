1f394a1112ce17ed3502dbb191da34aa1b48f4ad
BroccoliSpring <Lensgcx@163.com>
2021-07-18 12:18:38 +0800

    bizDataExpand

--- main/src/layout/mixins/bizDataExpand.ts
+++ main/src/layout/mixins/bizDataExpand.ts
@@ -1,8 +1,12 @@
+import { Component, Mixins } from 'vue-property-decorator';
+
+
 /**
  * 业务接口数据拓展 - 在全局请求获取部分业务配置数据
  */
-export default {
+@Component
+export default class bizDataExpand extends Mixins() {
   created() {
     this.$store.dispatch('getBizExpandData');
-  },
+  }
 };
