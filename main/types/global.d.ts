cce88e2fc4a4c89f74d185252e8f71a498214a45
BroccoliSpring <Lensgcx@163.com>
2021-07-18 12:19:38 +0800

    Update global.d.ts

--- main/types/global.d.ts
+++ main/types/global.d.ts
@@ -1,31 +1,35 @@
-// declare module '*.svg' {
-//   interface Svg {
-//     content: string;
-//     id: string;
-//     viewBox: string;
-//     node: any;
-//   }
-//
-//   const svg: Svg;
-//   export default svg;
-// }
-//
-// declare module '*.png' {
-//   const png: string;
-//   export default png;
-// }
-//
-// declare module '*.mp3' {
-//   const mp3: string;
-//   export default mp3;
-// }
-//
-// declare module '*.mp4' {
-//   const mp3: string;
-//   export default mp4;
-// }
-//
-// declare module '*.gif' {
-//   const png: string;
-//   export default png;
-// }
+declare module '*.ts'
+declare module '*.js'
+declare module '*.ts'
+
+declare module '*.svg' {
+  interface Svg {
+    content: string;
+    id: string;
+    viewBox: string;
+    node: any;
+  }
+
+  const svg: Svg;
+  export default svg;
+}
+
+declare module '*.png' {
+  const png: string;
+  export default png;
+}
+
+declare module '*.mp3' {
+  const mp3: string;
+  export default mp3;
+}
+
+declare module '*.mp4' {
+  const mp3: string;
+  export default mp4;
+}
+
+declare module '*.gif' {
+  const png: string;
+  export default png;
+}
