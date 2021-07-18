5587ad5501429458ea921099deffc89eee488a79
BroccoliSpring <Lensgcx@163.com>
2021-07-18 12:18:22 +0800

    Create index.ts

---
+++ main/src/layout/index.ts
@@ -0,0 +1,9 @@
+
+
+export { default as nsHeader } from "./components/NS-header/index.vue";
+export { default as bizSideMenu } from './components/NS-nav-menu/NS-side-menu/index.vue';
+
+export { default as bizTabsViews } from './components/NS-biz-page-tabs/index.vue';
+
+export { default as headerSlotLeft } from './components/NS-header-slot/NS-header-slot-left.vue';
+export { default as headerSlotRight } from "./components/NS-header-slot/NS-header-slot-right.vue";
