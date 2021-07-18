f65f9048e034e56b68a3709354a8204fa40a1fc3
BroccoliSpring <Lensgcx@163.com>
2021-07-18 12:18:18 +0800

    Delete index.js

--- main/src/layout/index.js
+++
@@ -1,7 +0,0 @@
-export { default as nsHeader } from './components/NS-header';
-export { default as bizSideMenu } from './components/NS-nav-menu/NS-side-menu';
-
-export { default as bizTabsViews } from './components/NS-biz-page-tabs';
-
-export { default as headerSlotLeft } from './components/NS-header-slot/NS-header-slot-left';
-export { default as headerSlotRight } from './components/NS-header-slot/NS-header-slot-right';
